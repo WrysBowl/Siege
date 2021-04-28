7272f421ff8c27e45d09758af4d4b36c23e15bf9
Ghqst <legobikes@outlook.com>
2021-04-28 14:18:16 -0400

    Add debug item that should actually work

--- core/src/main/java/net/siegerpg/siege/core/listeners/PlayerJoinListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/PlayerJoinListener.java
@@ -3,6 +3,8 @@ package net.siegerpg.siege.core.listeners;
 import net.siegerpg.siege.core.Core;
 import net.siegerpg.siege.core.informants.Scoreboard;
 import net.siegerpg.siege.core.informants.Tablist;
+import net.siegerpg.siege.core.items.CustomItem;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.Shank;
 import net.siegerpg.siege.core.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -78,6 +80,9 @@ public class PlayerJoinListener implements Listener {
             }
         }
         */
+        CustomItem shank = new Shank(100);
+        shank.updateMeta(false);
+        player.getInventory().addItem(shank.getItem());
 
         player.teleport(Core.plugin().getServer().getWorld("SiegeHub").getSpawnLocation());
     }
