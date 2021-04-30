dff6b4e39d07a6bc956cbd76aebb62832cb48cd9
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-04-30 11:19:08 +0200

    Made Levels.java use HikariCP

--- core/src/main/java/net/siegerpg/siege/core/utils/Levels.java
+++ core/src/main/java/net/siegerpg/siege/core/utils/Levels.java
@@ -1,15 +1,19 @@
 package net.siegerpg.siege.core.utils;
 
+import net.siegerpg.siege.core.database.DatabaseManager;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Levels {
     public static double calculateRequiredExperience(short level) {
-        return Math.pow(level+3, 3);
+        return Math.pow(level + 3, 3);
     }
-    
+
     public static void levelCalculate(OfflinePlayer player) {
         short level = getLevel(player);
         int exp = getExp(player);
@@ -23,7 +27,7 @@ public class Levels {
         if (getLevel(player) != level) {
             setLevel(player, level); //When while loop is finished, set the temp level variable to player's level
         }
-    
+
         if (player.isOnline()) {
             ((Player) player).setLevel(level);
             ((Player) player).setExp((float) (exp / calculateRequiredExperience(level)));
@@ -35,145 +39,96 @@ public class Levels {
     }
 
     public static short getLevel(OfflinePlayer player) {
-        return (short) player.getPlayer().getLevel(); //TEMP Remove when fixed
-
-        /*
-        Connection connection = DatabaseManager.INSTANCE.getConnection();
         try {
+            Connection connection = DatabaseManager.INSTANCE.getConnection();
             PreparedStatement statement = connection.prepareStatement("SELECT level FROM userData WHERE uuid=?");
             statement.setString(1, player.getUniqueId().toString());
             ResultSet query = statement.executeQuery();
             query.next();
-            short level = query.getShort("level");
-            DbManager.releaseConnection(connection);
-            statement.close();
-            return level;
+            return query.getShort("level");
         } catch (SQLException e) {
             e.printStackTrace();
             return 0;
         }
-
-        */
     }
 
     public static void setLevel(OfflinePlayer player, short level) {
-        // TODO: Reimplement
-        /*
-        new Thread(() -> {
-            Connection connection = null;
-            try {
-                connection = DatabaseManager.INSTANCE.getConnection();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                PreparedStatement statement = connection.prepareStatement("UPDATE userData SET level=? WHERE uuid=?");
-                statement.setShort(1, level);
-                statement.setString(2, player.getUniqueId().toString());
-                statement.executeUpdate();
-                DbManager.releaseConnection(connection);
-                if (player.isOnline()) ((Player) player).setLevel(level);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }).start();
-        */
+        try {
+            Connection connection = DatabaseManager.INSTANCE.getConnection();
+            PreparedStatement statement = connection.prepareStatement("UPDATE userData SET level=? WHERE uuid=?");
+            statement.setShort(1, level);
+            statement.setString(2, player.getUniqueId().toString());
+            statement.executeUpdate();
+            if (player.isOnline()) ((Player) player).setLevel(level);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void addLevel(OfflinePlayer player, short level) {
-        // TODO: Reimplement
-        /*
-        new Thread(() -> {
-            Connection connection = null;
-            try {
-                connection = DatabaseManager.INSTANCE.getConnection();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                PreparedStatement statement = connection.prepareStatement("UPDATE userData SET level=level+? WHERE uuid=?");
-                statement.setShort(1, level);
-                statement.setString(2, player.getUniqueId().toString());
-                statement.executeUpdate();
-                DbManager.releaseConnection(connection);
-                levelCalculate(player);
-                if (player.isOnline()) ((Player) player).setLevel(level);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }).start();
-        */
+        try {
+            Connection connection = DatabaseManager.INSTANCE.getConnection();
+            PreparedStatement statement = connection.prepareStatement("UPDATE userData SET level=level+? WHERE uuid=?");
+            statement.setShort(1, level);
+            statement.setString(2, player.getUniqueId().toString());
+            statement.executeUpdate();
+            levelCalculate(player);
+            if (player.isOnline()) ((Player) player).setLevel(level);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Integer getExp(OfflinePlayer player) {
-        return Math.round(player.getPlayer().getExp());
-        /*
-        Connection connection = DatabaseManager.INSTANCE.getConnection();
         try {
+            Connection connection = DatabaseManager.INSTANCE.getConnection();
+
             PreparedStatement statement = connection.prepareStatement("SELECT experience FROM userData WHERE uuid=?");
             statement.setString(1, player.getUniqueId().toString());
             ResultSet query = statement.executeQuery();
             query.next();
             int exp = query.getInt("experience");
-            DbManager.releaseConnection(connection);
             return exp;
         } catch (SQLException e) {
             e.printStackTrace();
             return 0;
         }
-        */
+
     }
 
     public static void setExp(OfflinePlayer player, int exp) {
-        // TODO: Reimplement
-        /*
-        new Thread(() -> {
-            Connection connection = null;
-            try {
-                connection = DatabaseManager.INSTANCE.getConnection();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            try {
-                PreparedStatement statement = connection.prepareStatement("UPDATE userData SET experience=? WHERE uuid=?");
-                statement.setInt(1, exp);
-                statement.setString(2, player.getUniqueId().toString());
-                statement.executeUpdate();
-                DbManager.releaseConnection(connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }).start();
-        */
+        try {
+            Connection connection = DatabaseManager.INSTANCE.getConnection();
+            PreparedStatement statement = connection.prepareStatement("UPDATE userData SET experience=? WHERE uuid=?");
+            statement.setInt(1, exp);
+            statement.setString(2, player.getUniqueId().toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public static void addExp(OfflinePlayer player, int exp) {
         addExp(player, exp, "");
     }
-    
+
     public static void addExp(OfflinePlayer player, int exp, String message) {
-        // TODO: Reimplement
-        /*
-        new Thread(() -> {
-            Connection connection = DbManager.getConnection();
-            try {
-                PreparedStatement statement = connection.prepareStatement("UPDATE userData SET experience=experience+? WHERE uuid=?");
-                statement.setInt(1, exp);
-                statement.setString(2, player.getUniqueId().toString());
-                statement.executeUpdate();
-                DbManager.releaseConnection(connection);
-                levelCalculate(player);
-                if (!message.equals("")) sendExpMessage(player, exp, message);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }).start();
-         */
+        try {
+            Connection connection = DatabaseManager.INSTANCE.getConnection();
+            PreparedStatement statement = connection.prepareStatement("UPDATE userData SET experience=experience+? WHERE uuid=?");
+            statement.setInt(1, exp);
+            statement.setString(2, player.getUniqueId().toString());
+            statement.executeUpdate();
+            levelCalculate(player);
+            if (!message.equals("")) sendExpMessage(player, exp, message);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public static void sendExpMessage(OfflinePlayer player, int exp, String reason) {
         if (!player.isOnline()) return;
         Player onlinePlayer = (Player) player;
-        onlinePlayer.sendMessage(Utils.tacc("&a+"+exp+" EXP &7&o"+reason));
+        onlinePlayer.sendMessage(Utils.tacc("&a+" + exp + " EXP &7&o" + reason));
     }
 }
