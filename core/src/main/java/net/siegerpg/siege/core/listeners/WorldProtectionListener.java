d4956ee1c3c5b5babccec648e86c0697b9a09bcc
Wryz <57204327+Wryz@users.noreply.github.com>
2021-05-02 13:51:31 -0400

    Interaction with doors enabled
    Players can interact with doors now

--- core/src/main/java/net/siegerpg/siege/core/listeners/WorldProtectionListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/WorldProtectionListener.java
@@ -2,11 +2,14 @@ package net.siegerpg.siege.core.listeners;
 
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.block.data.type.Door;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.Objects;
 
 public class WorldProtectionListener implements Listener {
     
@@ -27,6 +30,9 @@ public class WorldProtectionListener implements Listener {
     public void openDeniedBlocks(PlayerInteractEvent e) {
         if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             if (e.getPlayer().getGameMode().equals(GameMode.SURVIVAL) || e.getPlayer().getGameMode().equals(GameMode.ADVENTURE)) {
+                if (Objects.requireNonNull(e.getClickedBlock()).getBlockData() instanceof Door) {
+                    return;
+                }
                 e.setCancelled(true);
             }
         }
