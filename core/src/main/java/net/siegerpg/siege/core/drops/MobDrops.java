475dec86737ef6a19a46cf62ae7ecb2c568f308a
Wryz <57204327+Wryz@users.noreply.github.com>
2021-05-01 21:22:18 -0400

    Passive Mob Drop Table

--- core/src/main/java/net/siegerpg/siege/core/drops/MobDrops.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/MobDrops.java
@@ -68,7 +68,30 @@ public enum MobDrops {
             {Bone.Companion.tier(1).getUpdatedItem(false), 2.0},
             {new WoodenSword(Utils.randRarity()).getUpdatedItem(false), 5.0},
             {new Drumstick(0).getUpdatedItem(false), 15.0}
-    }, new Integer[]{2, 5}, new Integer[]{3, 6});
+    }, new Integer[]{2, 5}, new Integer[]{3, 6}),
+    FeatheredMeat(new Object[][]{
+        {Feather.Companion.tier(1).getUpdatedItem(false), 1.0},
+        {new Drumstick(0).getUpdatedItem(false), 35.0}
+    }, new Integer[]{2, 5}, new Integer[]{3, 6}),
+    Porky(new Object[][]{
+            {new Drumstick(0).getUpdatedItem(false), 100.0}
+    }, new Integer[]{5, 8}, new Integer[]{5, 7}),
+    Wooly(new Object[][]{
+            {new Drumstick(0).getUpdatedItem(false), 30.0},
+            {Wool.Companion.tier(1).getUpdatedItem(false), 60.0}
+    }, new Integer[]{8, 10}, new Integer[]{6, 8}),
+    MooMoo(new Object[][]{
+            {new Drumstick(0).getUpdatedItem(false), 100.0},
+            {Leather.Companion.tier(1).getUpdatedItem(false), 60.0}
+    }, new Integer[]{8, 12}, new Integer[]{6, 10}),
+    Pigeon(new Object[][]{
+            {new Drumstick(0).getUpdatedItem(false), 25.0},
+            {Feather.Companion.tier(1).getUpdatedItem(false), 60.0}
+    }, new Integer[]{1, 3}, new Integer[]{3, 5}),
+    Sushi(new Object[][]{
+            {new Drumstick(0).getUpdatedItem(false), 20.0},
+    }, new Integer[]{0, 2}, new Integer[]{0, 2})
+    ;
     private final Object[][] rewards;
     private final Integer[] numGold;
     private final Integer[] numExp;
