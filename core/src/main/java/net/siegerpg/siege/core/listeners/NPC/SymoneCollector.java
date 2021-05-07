b6416242d57505676c190f255eb57c19da48887e
Wryz <57204327+Wryz@users.noreply.github.com>
2021-05-07 01:08:45 -0400

    Symone the Collector Menu
    Added new collector NPC that displays all items in the game
    
    Currently added the menu and initialized the light melee list var

---
+++ core/src/main/java/net/siegerpg/siege/core/listeners/NPC/SymoneCollector.java
@@ -0,0 +1,249 @@
+package net.siegerpg.siege.core.listeners.NPC;
+
+import net.kyori.adventure.text.Component;
+import net.siegerpg.siege.core.Core;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.*;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.light.*;
+import net.siegerpg.siege.core.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class SymoneCollector implements Listener {
+
+    /**
+     * Loop through the lore parameter in the setOriginLore function and add the lore to the item
+     */
+
+    public static ArrayList<ItemStack> lightMeleeList = new ArrayList<>(){
+        {
+            add(setOriginLore(new Twig(50).getUpdatedItem(true),
+                    Utils.parse("<gold>Given when you first"),
+                    Utils.parse("<gold>join the server")));
+            add(setOriginLore(new StickyStick(50).getUpdatedItem(true),
+                    Utils.parse("<dark_aqua>Crafting"),
+                    Utils.parse("<dark_aqua>Slime \u272A\u272A x2"),
+                    Utils.parse("<dark_aqua>Stick \u272A\u272A x2")));
+            add(setOriginLore(new Spade(50).getUpdatedItem(true),
+                    Utils.parse("<dark_aqua>Crafting"),
+                    Utils.parse("<dark_aqua>Stick \u272A\u272A\u272A x1"),
+                    Utils.parse("<dark_aqua>Pebble \u272A\u272A x1")));
+            add(setOriginLore(new Shovel(50).getUpdatedItem(true),
+                    Utils.parse("<dark_aqua>Crafting"),
+                    Utils.parse("<dark_aqua>Stick \u272A\u272A\u272A x2"),
+                    Utils.parse("<dark_aqua>Pebble \u272A\u272A x1")));
+            add(setOriginLore(new Shank(50).getUpdatedItem(true),
+                    Utils.parse("<yellow>Bandit drop")));
+            add(setOriginLore(new Dagger(50).getUpdatedItem(true),
+                    Utils.parse("<yellow>Bandit drop")));
+            add(setOriginLore(new WoodenSword(50).getUpdatedItem(true),
+                    Utils.parse("<yellow>Wild Fox drop")));
+            add(setOriginLore(new ScrapShard(50).getUpdatedItem(true),
+                    Utils.parse("<dark_aqua>Crafting"),
+                    Utils.parse("<dark_aqua>Metal Scrap \u272A\u272A\u272A x2")));
+            add(setOriginLore(new SplinteredBone(50).getUpdatedItem(true),
+                    Utils.parse("<dark_aqua>Crafting"),
+                    Utils.parse("<dark_aqua>Bone \u272A\u272A\u272A x2"),
+                    Utils.parse("<yellow>Bandit drop")));
+            add(setOriginLore(new RefinedDagger(50).getUpdatedItem(true),
+                    Utils.parse("<dark_aqua>Crafting"),
+                    Utils.parse("<dark_aqua>Stick \u272A\u272A\u272A x1"),
+                    Utils.parse("<dark_aqua>Refined Metal \u272A\u272A\u272A x1")));
+        }
+    };
+
+    public static ArrayList<ItemStack> heavyMeleeList = new ArrayList<>(){
+        {
+            add(new Club(50).getUpdatedItem(true));
+            add(new GiantClub(50).getUpdatedItem(true));
+            add(new FemurBone(50).getUpdatedItem(true));
+            add(new StoneAxe(50).getUpdatedItem(true));
+            add(new DoubleBladedAxe(50).getUpdatedItem(true));
+            add(new GreatSword(50).getUpdatedItem(true));
+            add(new WarHammer(50).getUpdatedItem(true));
+            add(new IronAxe(50).getUpdatedItem(true));
+            add(new Clobber(50).getUpdatedItem(true));
+            add(new EarthernHammer(50).getUpdatedItem(true));
+        }
+    };
+
+    public static ArrayList<ItemStack> armorList = new ArrayList<>(){
+        {
+            add(new Twig(50).getUpdatedItem(true));
+            add(new StickyStick(50).getUpdatedItem(true));
+            add(new Spade(50).getUpdatedItem(true));
+            add(new Shovel(50).getUpdatedItem(true));
+            add(new Shank(50).getUpdatedItem(true));
+            add(new Dagger(50).getUpdatedItem(true));
+            add(new WoodenSword(50).getUpdatedItem(true));
+            add(new ScrapShard(50).getUpdatedItem(true));
+            add(new SplinteredBone(50).getUpdatedItem(true));
+            add(new RefinedDagger(50).getUpdatedItem(true));
+        }
+    };
+
+    public static ArrayList<ItemStack> wandsList = new ArrayList<>(){
+        {
+            add(new Twig(50).getUpdatedItem(true));
+            add(new StickyStick(50).getUpdatedItem(true));
+            add(new Spade(50).getUpdatedItem(true));
+            add(new Shovel(50).getUpdatedItem(true));
+            add(new Shank(50).getUpdatedItem(true));
+            add(new Dagger(50).getUpdatedItem(true));
+            add(new WoodenSword(50).getUpdatedItem(true));
+            add(new ScrapShard(50).getUpdatedItem(true));
+            add(new SplinteredBone(50).getUpdatedItem(true));
+            add(new RefinedDagger(50).getUpdatedItem(true));
+        }
+    };
+
+    public static ArrayList<ItemStack> rangedList = new ArrayList<>(){
+        {
+            add(new Twig(50).getUpdatedItem(true));
+            add(new StickyStick(50).getUpdatedItem(true));
+            add(new Spade(50).getUpdatedItem(true));
+            add(new Shovel(50).getUpdatedItem(true));
+            add(new Shank(50).getUpdatedItem(true));
+            add(new Dagger(50).getUpdatedItem(true));
+            add(new WoodenSword(50).getUpdatedItem(true));
+            add(new ScrapShard(50).getUpdatedItem(true));
+            add(new SplinteredBone(50).getUpdatedItem(true));
+            add(new RefinedDagger(50).getUpdatedItem(true));
+        }
+    };
+
+    private static ItemStack setOriginLore(ItemStack item, Component... lore) {
+        List<Component> newLore = item.lore();
+        if (newLore == null) {return item;}
+        newLore.add(Utils.parse(" "));
+        newLore.add(Utils.parse("<aqua><italic>Origin"));
+        newLore.addAll(Arrays.asList(lore));
+        item.lore(newLore);
+        return item;
+    }
+
+    @EventHandler
+    public void onRightClickOnEntity(PlayerInteractEntityEvent e) {
+        if (e.getRightClicked().getName().contains(Utils.tacc("&6Symone"))) {
+            e.getPlayer().openInventory(getMenu(e.getPlayer()));
+        }
+    }
+
+    @EventHandler
+    public void guiClick(InventoryClickEvent e) {
+        if (!(e.getWhoClicked() instanceof Player)) {return;}
+        if (e.getWhoClicked().getMetadata("SymoneMenu").size() > 0 &&
+                Objects.equals(e.getWhoClicked().getMetadata("SymoneMenu").get(0).value(), e.getInventory())) {
+            clickMenu(e);
+            e.setCancelled(true);
+        }/* else if (e.getWhoClicked().getMetadata("SymoneLightMelee").size() > 0 &&
+                Objects.equals(e.getWhoClicked().getMetadata("SymoneLightMelee").get(0).value(), e.getInventory())) {
+            clickLightMelee(e);
+            e.setCancelled(true);
+        } else if (e.getWhoClicked().getMetadata("SymoneHeavyMelee").size() > 0 &&
+                Objects.equals(e.getWhoClicked().getMetadata("SymoneHeavyMelee").get(0).value(), e.getInventory())) {
+            clickHeavyMelee(e);
+            e.setCancelled(true);
+        } else if (e.getWhoClicked().getMetadata("SymoneArmor").size() > 0 &&
+                Objects.equals(e.getWhoClicked().getMetadata("SymoneArmor").get(0).value(), e.getInventory())) {
+            clickArmor(e);
+            e.setCancelled(true);
+        } else if (e.getWhoClicked().getMetadata("SymoneWand").size() > 0 &&
+                Objects.equals(e.getWhoClicked().getMetadata("SymoneWand").get(0).value(), e.getInventory())) {
+            clickWand(e);
+            e.setCancelled(true);
+        } else if (e.getWhoClicked().getMetadata("SymoneRanged").size() > 0 &&
+                Objects.equals(e.getWhoClicked().getMetadata("SymoneRanged").get(0).value(), e.getInventory())) {
+            clickRanged(e);
+            e.setCancelled(true);
+        }*/
+    }
+
+    private void clickMenu(InventoryClickEvent e) {
+        int slot = e.getSlot();
+        Player player = (Player) e.getWhoClicked();
+        /*
+        if (slot == 11) {
+            player.openInventory(getLightMelee(player));
+        } else if (slot == 12) {
+            player.openInventory(getHeavyMelee(player));
+        } else if (slot == 13) {
+            player.openInventory(getArmor(player));
+        } else if (slot == 14) {
+            player.openInventory(getWand(player));
+        } else if (slot == 15) {
+            player.openInventory(getRanged(player));
+        }*/
+    }
+
+    private Inventory getMenu(Player player) {
+        Inventory gui = Bukkit.createInventory(null, 27, "Symone's Collection");
+
+        //Fill in the GUI
+        ItemStack filler = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+        for (int i = 0; i < gui.getSize(); i++) {
+            gui.setItem(i, filler);
+        }
+
+        //Creating Light Melee Icon
+        ItemStack lightMelee = new ItemStack (Material.WOODEN_SWORD);
+        ItemMeta lightMeleeMeta = lightMelee.getItemMeta();
+        lightMeleeMeta.displayName(Utils.lore("<red>Light Melee"));
+        lightMeleeMeta.removeAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE);
+        lightMeleeMeta.removeAttributeModifier(Attribute.GENERIC_ATTACK_SPEED);
+        lightMelee.setItemMeta(lightMeleeMeta);
+
+        //Creating Heavy Melee Icon
+        ItemStack heavyMelee = new ItemStack (Material.WOODEN_AXE);
+        ItemMeta heavyMeleeMeta = heavyMelee.getItemMeta();
+        heavyMeleeMeta.displayName(Utils.lore("<dark_red>Heavy Melee"));
+        heavyMeleeMeta.removeAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE);
+        heavyMeleeMeta.removeAttributeModifier(Attribute.GENERIC_ATTACK_SPEED);
+        heavyMelee.setItemMeta(heavyMeleeMeta);
+
+        //Creating Armor Icon
+        ItemStack armor = new ItemStack (Material.IRON_CHESTPLATE);
+        ItemMeta armorMeta = armor.getItemMeta();
+        armorMeta.displayName(Utils.lore("<dark_aqua>Armor"));
+        armorMeta.removeAttributeModifier(Attribute.GENERIC_ARMOR);
+        armor.setItemMeta(armorMeta);
+
+        //Creating Wand Icon
+        ItemStack wand = new ItemStack (Material.WOODEN_HOE);
+        ItemMeta wandMeta = wand.getItemMeta();
+        wandMeta.displayName(Utils.lore("<dark_green>Wands"));
+        wandMeta.removeAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE);
+        wandMeta.removeAttributeModifier(Attribute.GENERIC_ATTACK_SPEED);
+        wand.setItemMeta(wandMeta);
+
+        //Creating Ranged Icon
+        ItemStack ranged = new ItemStack (Material.BOW);
+        ItemMeta rangedMeta = ranged.getItemMeta();
+        rangedMeta.displayName(Utils.lore("<green>Ranged Weapons"));
+        ranged.setItemMeta(rangedMeta);
+
+        //This is where you decide what slot the item goes into
+        gui.setItem(11, lightMelee);
+        gui.setItem(12, heavyMelee);
+        gui.setItem(13, armor);
+        gui.setItem(14, wand);
+        gui.setItem(15, ranged);
+
+        player.setMetadata("SymoneMenu", new FixedMetadataValue(Core.plugin(), gui));
+        return gui;
+    }
+}
