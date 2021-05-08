a781497c119f5c0ce3fc3d0564d33d3b4de0a039
Wryz <57204327+Wryz@users.noreply.github.com>
2021-05-08 18:40:35 -0400

    Stat Action Bar

--- core/src/main/java/net/siegerpg/siege/core/listeners/StatChangeListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/StatChangeListener.java
@@ -1,48 +1,32 @@
 package net.siegerpg.siege.core.listeners;
 
+import io.lumine.xikage.mythicmobs.utils.Players;
 import net.siegerpg.siege.core.Core;
-import net.siegerpg.siege.core.informants.StatVariables;
-import net.siegerpg.siege.core.items.CustomItem;
 import net.siegerpg.siege.core.items.CustomItemUtils;
 import net.siegerpg.siege.core.items.enums.StatTypes;
 import net.siegerpg.siege.core.listeners.ArmorEquip.ArmorEquipEvent;
+import net.siegerpg.siege.core.utils.Utils;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 
-public class StatChangeListener implements Listener {
+import java.util.HashMap;
 
-    /*
-    Bukkit.getLogger().info("ArmorEquipEvent - " + event.getMethod());
-        Bukkit.getLogger().info("Type: " + event.getType());
-        Bukkit.getLogger().info("New: " + (event.getNewArmorPiece() != null ? event.getNewArmorPiece().getType() : "null"));
-        Bukkit.getLogger().info("Old: " + (event.getOldArmorPiece() != null ? event.getOldArmorPiece().getType() : "null"));
-        boolean test = true;
-        if(test){
-            // Does a test where if you start in survival without a helmet on you SHOULD end up in survival without the helmet on, or adventure mode if helmet is on.
-            // Used to make sure spam clicking doesn't mess up, no clue if a better test exists.
-            if(event.getOldArmorPiece() != null && event.getOldArmorPiece().getType().equals(Material.DIAMOND_HELMET)){
-                event.getPlayer().setGameMode(event.getPlayer().getGameMode().equals(GameMode.ADVENTURE) ? GameMode.SURVIVAL : GameMode.ADVENTURE);
-            }
-            if(event.getNewArmorPiece() != null && event.getNewArmorPiece().getType().equals(Material.DIAMOND_HELMET)){
-                event.getPlayer().setGameMode(event.getPlayer().getGameMode().equals(GameMode.ADVENTURE) ? GameMode.SURVIVAL : GameMode.ADVENTURE);
-            }
-            Bukkit.getLogger().info("New Gamemode: " + event.getPlayer().getGameMode());
-        }
-     */
+public class StatChangeListener implements Listener, Runnable {
+
+    public static HashMap<Player, Double> playerHealth = new HashMap<>();
+    public static HashMap<Player, Double> playerToughness = new HashMap<>();
 
     @EventHandler
     public void onEquip(ArmorEquipEvent e){
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Core.plugin(), () -> {
-            StatVariables.playerHealth.put(
+            playerHealth.put(
                     e.getPlayer(),
                     CustomItemUtils.INSTANCE.getPlayerStat(e.getPlayer(), StatTypes.HEALTH));
 
-            StatVariables.playerToughness.put(
+            playerToughness.put(
                     e.getPlayer(),
                     CustomItemUtils.INSTANCE.getPlayerStat(e.getPlayer(), StatTypes.TOUGHNESS));
         }, 2);
@@ -52,14 +36,31 @@ public class StatChangeListener implements Listener {
     public void toolSwitch(PlayerItemHeldEvent e) {
         if (CustomItemUtils.INSTANCE.getCustomItem(e.getPlayer().getInventory().getItemInMainHand()) != null) {
             Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Core.plugin(), () -> {
-                StatVariables.playerHealth.put(
+                playerHealth.put(
                         e.getPlayer(),
                         CustomItemUtils.INSTANCE.getPlayerStat(e.getPlayer(), StatTypes.HEALTH));
 
-                StatVariables.playerToughness.put(
+                playerToughness.put(
                         e.getPlayer(),
                         CustomItemUtils.INSTANCE.getPlayerStat(e.getPlayer(), StatTypes.TOUGHNESS));
             }, 2);
         }
+    }
+
+    public static void statBarDisplayTask() {
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Core.plugin(), () -> {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                Double health = playerHealth.get(p);
+                Double toughness = playerToughness.get(p);
+                p.sendActionBar(Utils.parse("<red>"
+                        + CustomItemUtils.INSTANCE.getCustomHealth(p) + "<dark_red>/<red>" + health
+                        + "       <blue>" + toughness));
+            }
+        }, 0, 40);
+    }
+
+    @Override
+    public void run() {
+
     }
 }
