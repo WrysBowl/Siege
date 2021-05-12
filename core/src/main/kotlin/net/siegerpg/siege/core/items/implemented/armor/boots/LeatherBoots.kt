4e96c3cd35943eb77a1128ad391f0d99de03dde9
MrCloss <guycloudy@gmail.com>
2021-05-12 15:34:00 -0400

    Added Hardened Leather Armor set

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/boots/LeatherBoots.kt
@@ -0,0 +1,62 @@
+package net.siegerpg.siege.core.items.implemented.armor.boots
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.implemented.misc.wands.GlowingTwig
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.armor.CustomBoots
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class LeatherBoots() : CustomBoots(
+    name = "Hardened Leather Boots",
+    customModelData = 1,
+    description = listOf("Bootleg spurs!"),
+    levelRequirement = 17,
+    material = Material.LEATHER_BOOTS,
+    baseStats = CustomItemUtils.statMap(health = 20.0, toughness = 30.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Leather.tier(2))
+            s3(Leather.tier(2))
+            s4(Leather.tier(2))
+            s6(Leather.tier(2))
+            item { player, b ->
+                val newItem = LeatherBoots(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+        recipe {
+            shaped = true
+            s4(Leather.tier(2))
+            s6(Leather.tier(2))
+            s7(Leather.tier(2))
+            s9(Leather.tier(2))
+            item { player, b ->
+                val newItem = LeatherBoots(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    }
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
