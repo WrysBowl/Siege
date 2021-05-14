fa5d4e4c9d3db140120f5b1cc95b5e131b88636b
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-05-14 11:53:56 +0200

    Fixed players not being added to db

--- core/src/main/java/net/siegerpg/siege/core/listeners/PlayerJoinListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/PlayerJoinListener.java
@@ -1,7 +1,7 @@
 package net.siegerpg.siege.core.listeners;
 
-import kotlin.Pair;
 import net.siegerpg.siege.core.Core;
+import net.siegerpg.siege.core.database.DatabaseManager;
 import net.siegerpg.siege.core.informants.Scoreboard;
 import net.siegerpg.siege.core.informants.Tablist;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.StrengthGem;
@@ -11,28 +11,33 @@ import net.siegerpg.siege.core.utils.Levels;
 import net.siegerpg.siege.core.utils.Utils;
 import net.siegerpg.siege.core.utils.VaultHook;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PlayerJoinListener implements Listener {
-    
+
     @EventHandler
     public void connectEvent(AsyncPlayerPreLoginEvent e) {
         new BukkitRunnable() { // We create a runnable to run asynchronously (on another thread, not the main one, so that the server won't lag if this one does)
             @Override
             public void run() {
                 String ip = e.getAddress().getHostAddress();
-                Connection conn = DbManager.getConnection(); // Get the connection from the pool
                 // Add user ips to the db (So that we can in the future find all alts of an user)
-                try {
-                    PreparedStatement stat = conn.prepareStatement("SELECT * FROM ipData WHERE uuid=? AND ip=?");
+                try (Connection conn = DatabaseManager.INSTANCE.getConnection()) {
+                    PreparedStatement stat = conn.prepareStatement("SELECT ip FROM ipData WHERE uuid=? AND ip=?");
                     stat.setString(1, e.getUniqueId().toString());
                     stat.setString(2, ip);
                     ResultSet set = stat.executeQuery();
-                    if (set.getFetchSize() < 1) {
+                    if (!set.isBeforeFirst()) {
                         PreparedStatement statement = conn.prepareStatement("INSERT INTO ipData (uuid, ip) VALUES (?, ?)");
                         statement.setString(1, e.getUniqueId().toString());
                         statement.setString(2, ip);
@@ -44,22 +49,10 @@ public class PlayerJoinListener implements Listener {
                     userData.executeUpdate();
                 } catch (SQLException ignored) {
                 }
-                DbManager.releaseConnection(conn); // Release the connection so it can be used by others
             }
         }.runTaskAsynchronously(Core.plugin());
     }
-    
 
-    /*
-    @EventHandler
-    public void loginEvent(PlayerLoginEvent event) {
-        Player p = event.getPlayer();
-        if(!p.hasPlayedBefore()) {
-
-        }
-    }
-
-     */
 
     @EventHandler
     public void joinEvent(PlayerJoinEvent event) {
@@ -81,15 +74,14 @@ public class PlayerJoinListener implements Listener {
             player.getInventory().addItem(new Twig(Utils.randRarity()).getUpdatedItem(false));
             VaultHook.econ.depositPlayer(player, 100.0);
             Levels.INSTANCE.setLevel(player, (short) 1);
-        }  else if (Levels.INSTANCE.getExpLevel(player).getFirst() < 1) {
-                Levels.INSTANCE.setLevel(player, (short) 1);
+        } else if (Levels.INSTANCE.getExpLevel(player).getFirst() < 1) {
+            Levels.INSTANCE.setLevel(player, (short) 1);
         }
 
         for (Player p : Bukkit.getOnlinePlayers()) {
             Scoreboard.updateScoreboard(p);
             Tablist.tablistUpdate(p);
         }
-
 
 
         if (player.getName().equals("Sumowu")) {
