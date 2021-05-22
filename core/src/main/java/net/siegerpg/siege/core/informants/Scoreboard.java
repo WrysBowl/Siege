1830633f835547d59907f54b5ee58390b2ab307e
Wryz <57204327+Wryz@users.noreply.github.com>
2021-05-22 03:05:58 -0400

    Scoreboard gold now formatted

--- core/src/main/java/net/siegerpg/siege/core/informants/Scoreboard.java
+++ core/src/main/java/net/siegerpg/siege/core/informants/Scoreboard.java
@@ -22,13 +22,14 @@ public class Scoreboard {
         float reqExp = Levels.INSTANCE.calculateRequiredExperience(levelExp.getFirst());
         double division = exp/reqExp;
         double levelPercent = Utils.round(division, 3);
+        String gold = String.format("%,d", (int) VaultHook.econ.getBalance(p));
         o.getScore(" ").setScore(15);
 
         o.getScore(Utils.tacc("&6&lWorld &r&7") + p.getWorld().getName()).setScore(14);
 
         o.getScore(Utils.tacc("&6Profile " + VaultHook.perms.getPrimaryGroup(p) + " &7" + p.getName())).setScore(13);
         o.getScore(Utils.tacc("&7\u2560 Level &5" + level + " &d(" + Utils.round(levelPercent*100,2) + "%)")).setScore(12);
-        o.getScore(Utils.tacc("&7\u2560 Gold &e" + (int) VaultHook.econ.getBalance(p))).setScore(11);
+        o.getScore(Utils.tacc("&7\u2560 Gold &e" + gold)).setScore(11);
         o.getScore("  ").setScore(10);
         o.getScore(Utils.tacc("&7play.SiegeRPG.net")).setScore(4);
         p.setScoreboard(b);
