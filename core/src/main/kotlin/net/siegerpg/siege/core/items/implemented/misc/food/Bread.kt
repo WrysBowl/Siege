0d0516b251247029d472bbe300c5ee654dec6f9d
MrCloss <guycloudy@gmail.com>
2021-05-24 18:12:57 -0400

    Added Bread

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/food/Bread.kt
@@ -0,0 +1,34 @@
+package net.siegerpg.siege.core.items.implemented.misc.food
+
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.misc.CustomFood
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+
+class Bread() : CustomFood(
+    name = "Bread",
+    customModelData = 330009,
+    description = listOf("Doughy"),
+    levelRequirement = 0,
+    material = Material.BREAD,
+    recipeList = recipes {
+
+    },
+    health = 25,
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        this.deserialize()
+    }
+
+}
