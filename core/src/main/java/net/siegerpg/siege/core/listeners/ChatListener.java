56be1ace10368a452f9123a01a34301aaf4e628e
Blueyy <la.eaton@gmail.com>
2021-05-25 19:08:44 +0100

    Started Item!
    Cool dude

--- core/src/main/java/net/siegerpg/siege/core/listeners/ChatListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/ChatListener.java
@@ -2,10 +2,13 @@ package net.siegerpg.siege.core.listeners;
 
 import net.siegerpg.siege.core.utils.Levels;
 import net.siegerpg.siege.core.utils.Utils;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.List;
 
 public class ChatListener implements Listener {
 
@@ -20,6 +23,17 @@ public class ChatListener implements Listener {
             e.getPlayer().sendMessage(Utils.tacc("You can not send a empty message!"));
             e.setCancelled(true);
             return;
+        }
+        if (message.contains("[item]")) {
+
+            if (player.getInventory().getItemInMainHand().getType() != Material.AIR ) {
+
+                String name = player.getInventory().getItemInMainHand().getItemMeta().getDisplayName();
+
+                List<String> lore = player.getInventory().getItemInMainHand().getItemMeta().getLore();
+
+            }
+
         }
         e.setFormat(Utils.tacc(level + " " + prefix + " ") + Utils.tacc("&7%1$s &f%2$s"));
     }
