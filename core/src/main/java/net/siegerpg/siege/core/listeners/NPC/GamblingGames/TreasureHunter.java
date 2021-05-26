aaba66fa9faeecd04bc034eeb2056b82cc2d7d56
Wryz <57204327+Wryz@users.noreply.github.com>
2021-05-26 13:19:52 -0400

    Removed EXP reward and added +50 and +75 gold rewards

--- core/src/main/java/net/siegerpg/siege/core/listeners/NPC/GamblingGames/TreasureHunter.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/NPC/GamblingGames/TreasureHunter.java
@@ -1,71 +1,28 @@
 package net.siegerpg.siege.core.listeners.NPC.GamblingGames;
 
-import com.github.stefvanschie.inventoryframework.font.util.Font;
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
-import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
 import com.github.stefvanschie.inventoryframework.pane.Pane;
 import com.github.stefvanschie.inventoryframework.pane.StaticPane;
-import com.github.stefvanschie.inventoryframework.pane.component.Label;
 import net.siegerpg.siege.core.Core;
 import net.siegerpg.siege.core.informants.Scoreboard;
 import net.siegerpg.siege.core.utils.Levels;
 import net.siegerpg.siege.core.utils.Utils;
 import net.siegerpg.siege.core.utils.VaultHook;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TreasureHunter {
-    /**
-     * PSEUDOCODE
-     *
-     * First page: 5 rows
-     * 20. START description Costs 500 gold, NO REFUNDS -> second page
-     * 24. HELP description dig to your heart's content
-     * and hope you don't dig up a hidden bomb.
-     * If you close the gui, you keep all your rewards!
-     *
-     * Second page: 6 rows
-     * Generate list of random values from 0 to 4
-     * 0's are bombs
-     * 1's are 100 coins
-     * 2's are 100 exp
-     * 3's are nothing
-     * 4's are nothing
-     *
-     * Initialize goldReward and expReward var to store player rewards
-     *
-     * onGuiClick ->
-     *
-     *  if slot is a '0', then set all gui slots to
-     *  red glass pane named "<bold>YOU LOST" after 2 seconds
-     *  clear list
-     *  close inventory
-     *
-     *  if slot is a '1', add 100 to the goldReward and
-     *  set the slot to a gold coin named &e+100
-     *  send entity_experience_orb sound
-     *  if slot is a '2', add 100 to the expReward
-     *  set the slot to an exp bottle named &d+100
-     *  send block_stone_button_push sound
-     *
-     * onGuiClose ->
-     *  check if list is empty -> return
-     *  give player rewards from list
-     *  make NPC send player message "&6Bart: &7Rematch?!"
-     */
+
     public ArrayList<ItemStack> itemRewards = new ArrayList<>();
     public int goldRewards = 0;
     public int expRewards = 0;
@@ -76,7 +33,9 @@ public class TreasureHunter {
     private final StaticPane background = new StaticPane(0, 0, 9, 6, Pane.Priority.LOWEST);
     private final ItemStack filler = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
     private final ItemStack bomb = new ItemStack(Material.TNT);
-    private final ItemStack gold = new ItemStack(Material.SUNFLOWER);
+    private final ItemStack gold100 = new ItemStack(Material.SUNFLOWER);
+    private final ItemStack gold75 = new ItemStack(Material.SUNFLOWER);
+    private final ItemStack gold50 = new ItemStack(Material.SUNFLOWER);
     private final ItemStack exp = new ItemStack(Material.EXPERIENCE_BOTTLE);
 
     public TreasureHunter(Player player) {
@@ -97,7 +56,7 @@ public class TreasureHunter {
     }
     private void startGame() {
         for(int i = 0; i<(game.getRows()*9); i++) {
-            this.rewardTable.put(i, (int) (Math.random() * 5));
+            this.rewardTable.put(i, (int) (Math.random() * 7));
         }
 
         ItemMeta bombMeta = this.bomb.getItemMeta();
@@ -109,9 +68,17 @@ public class TreasureHunter {
         });
         this.bomb.setItemMeta(bombMeta);
 
-        ItemMeta goldMeta = this.gold.getItemMeta();
-        goldMeta.displayName(Utils.lore("<yellow>+100 Gold"));
-        this.gold.setItemMeta(goldMeta);
+        ItemMeta gold100Meta = this.gold100.getItemMeta();
+        gold100Meta.displayName(Utils.lore("<yellow>+100 Gold"));
+        this.gold100.setItemMeta(gold100Meta);
+
+        ItemMeta gold75Meta = this.gold75.getItemMeta();
+        gold75Meta.displayName(Utils.lore("<yellow>+75 Gold"));
+        this.gold75.setItemMeta(gold75Meta);
+
+        ItemMeta gold50Meta = this.gold50.getItemMeta();
+        gold50Meta.displayName(Utils.lore("<yellow>+50 Gold"));
+        this.gold50.setItemMeta(gold50Meta);
 
         ItemMeta expMeta = this.exp.getItemMeta();
         expMeta.displayName(Utils.lore("<light_purple>+100 EXP"));
@@ -137,13 +104,18 @@ public class TreasureHunter {
                 }
             }.runTaskLater(Core.plugin(), 80);
         } else if (getValue == 1) {
-            this.background.addItem(new GuiItem(this.gold), x, y);
+            this.background.addItem(new GuiItem(this.gold100), x, y);
             this.goldRewards += 100;
             this.game.setTitle(Utils.tacc("&a&lREWARDS&r &e+" + this.goldRewards + " Gold &d+" + this.expRewards + " EXP"));
             player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
         } else if (getValue == 2) {
-            this.background.addItem(new GuiItem(this.exp), x, y);
-            this.expRewards += 100;
+            this.background.addItem(new GuiItem(this.gold75), x, y);
+            this.goldRewards += 75;
+            this.game.setTitle(Utils.tacc("&a&lREWARDS&r &e+" + this.goldRewards + " Gold &d+" + this.expRewards + " EXP"));
+            player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+        } else if (getValue == 3) {
+            this.background.addItem(new GuiItem(this.gold50), x, y);
+            this.goldRewards += 50;
             this.game.setTitle(Utils.tacc("&a&lREWARDS&r &e+" + this.goldRewards + " Gold &d+" + this.expRewards + " EXP"));
             player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
         } else {
