6f6073b92e1b78fb201d8a2f28a6ca8042b20ce7
Wryz <57204327+Wryz@users.noreply.github.com>
2021-06-05 17:40:49 -0400

    Fixed bug where wands had player level added

--- core/src/main/kotlin/net/siegerpg/siege/core/listeners/CustomItemKotlinListener.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/listeners/CustomItemKotlinListener.kt
@@ -251,7 +251,7 @@ class CustomItemKotlinListener : Listener, Runnable {
         CustomItemUtils.getCustomItem(item)?.let {
             if (it is CustomWand) {
 
-                var dmg = it.baseStats[StatTypes.STRENGTH]!! + player.level
+                var dmg = CustomItemUtils.getPlayerStat(player, StatTypes.STRENGTH)
                 if (player.level < CustomItemUtils.getCustomItem(item)?.levelRequirement!!) dmg = 1.0
 
                 //MAKE THIS EFFICIENT
