429cb68f2fca8886cf0ea03232b5559aa6f89cb4
MrCloss <guycloudy@gmail.com>
2021-06-06 23:17:13 -0400

    ADded
    Strong Sticky Stick

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/weapons/melee/light/stickySticks/StrongStickyStick.kt
@@ -0,0 +1,50 @@
+package net.siegerpg.siege.core.items.implemented.weapons.melee.light.stickySticks
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Stick
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Bone
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Feather
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Slime
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.weapons.CustomMeleeWeapon
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class StrongStickyStick() : CustomMeleeWeapon(
+    name = "Strong Sticky Stick",
+    customModelData = 110002,
+    description = listOf("Globs of slime on a stick"),
+    levelRequirement = 5,
+    material = Material.WOODEN_SWORD,
+    baseStats = CustomItemUtils.statMap(strength = 10.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Stick.tier(2))
+            s4(Stick.tier(2))
+            s2(Slime.tier(2))
+            s5(Bone.tier(1))
+            item { player, b ->
+                val newItem = LuckyStickyStick(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    attackSpeed = 1.6
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/weapons/melee/light/stickySticks/ToughStickyStick.kt
@@ -0,0 +1,4 @@
+package net.siegerpg.siege.core.items.implemented.weapons.melee.light.stickySticks
+
+class ToughStickyStick {
+}
