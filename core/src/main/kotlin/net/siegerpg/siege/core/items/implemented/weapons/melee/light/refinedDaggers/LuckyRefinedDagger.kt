ff6f39a1931f4e82d1bb85ce3214d1446f00cc08
Wryz <57204327+Wryz@users.noreply.github.com>
2021-06-08 15:16:44 -0400

    Refined Dagger implementation

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/weapons/melee/light/refinedDaggers/LuckyRefinedDagger.kt
@@ -0,0 +1,54 @@
+package net.siegerpg.siege.core.items.implemented.weapons.melee.light
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.RefinedMetal
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Stick
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Wheat
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Feather
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.StoneAxe
+import net.siegerpg.siege.core.items.implemented.weapons.melee.light.splinteredBones.HealingSplinteredBone
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.weapons.CustomMeleeWeapon
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class LuckyRefinedDagger() : CustomMeleeWeapon(
+    name = "Lucky Refined Dagger",
+    customModelData = 110010,
+    description = listOf("A dagger, but shinier", "and more pointy"),
+    levelRequirement = 37,
+    material = Material.WOODEN_SWORD,
+    baseStats = CustomItemUtils.statMap(strength = 34.0, luck = 8.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(RefinedMetal.tier(2))
+            s2(RefinedMetal.tier(2))
+            s3(RefinedMetal.tier(2))
+            s4(RefinedMetal.tier(2))
+            s5(Stick.tier(3))
+            s6(Feather.tier(3))
+            item { player, b ->
+                val newItem = LuckyRefinedDagger(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    attackSpeed = 1.6
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/weapons/melee/light/refinedDaggers/ToughRefinedDagger.kt
@@ -0,0 +1,51 @@
+package net.siegerpg.siege.core.items.implemented.weapons.melee.light
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.weapons.CustomMeleeWeapon
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class ToughRefinedDagger() : CustomMeleeWeapon(
+    name = "Tough Refined Dagger",
+    customModelData = 110010,
+    description = listOf("A dagger, but shinier", "and more pointy"),
+    levelRequirement = 37,
+    material = Material.WOODEN_SWORD,
+    baseStats = CustomItemUtils.statMap(strength = 34.0, toughness = 60.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(RefinedMetal.tier(2))
+            s2(RefinedMetal.tier(2))
+            s3(RefinedMetal.tier(2))
+            s4(RefinedMetal.tier(2))
+            s5(Stick.tier(3))
+            s6(Pebble.tier(3))
+            s7(Pebble.tier(3))
+            item { player, b ->
+                val newItem = ToughRefinedDagger(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    attackSpeed = 1.6
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
