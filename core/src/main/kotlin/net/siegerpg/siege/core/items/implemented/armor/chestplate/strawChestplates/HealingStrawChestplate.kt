cb4912c34e299911b95fe15e63e857a51b0d9e96
Emily <lizemty36@gmail.com>
2021-06-10 16:04:10 -0400

    Straw Hats & Straw Chestplates

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/chestplate/strawChestplates/HealingStrawChestplate.kt
@@ -0,0 +1,55 @@
+package net.siegerpg.siege.core.items.implemented.armor.chestplate.strawChestplates
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.armor.CustomChestplate
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class HealingStrawChestplate() : CustomChestplate(
+    name = "Healing Straw Chestplate",
+    customModelData = 1,
+    description = listOf("Scarecrow..."),
+    levelRequirement = 4,
+    material = Material.LEATHER_CHESTPLATE,
+    baseStats = CustomItemUtils.statMap(health = 6.0, regeneration = 5.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Seed.tier(2))
+            s3(Seed.tier(2))
+            s4(Seed.tier(2))
+            s5(Wheat.tier(2))
+            s6(Wheat.tier(2))
+            s7(Wheat.tier(2))
+            s8(Wheat.tier(2))
+            s9(Wheat.tier(2))
+            s9(Wheat.tier(2))
+            item { player, b ->
+                val newItem = HealingStrawChestplate(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    leatherColor = Color.YELLOW
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/helmet/strawHelmets/HealingStrawHat.kt
@@ -0,0 +1,50 @@
+package net.siegerpg.siege.core.items.implemented.armor.helmet.strawHelmets
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class HealingStrawHat() : CustomHelmet(
+    name = "Healing Straw Hat",
+    customModelData = 1,
+    description = listOf("Farmer"),
+    levelRequirement = 3,
+    material = Material.LEATHER_HELMET,
+    baseStats = CustomItemUtils.statMap(health = 2.0, regeneration = 3.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Seed.tier(2))
+            s2(Seed.tier(2))
+            s3(Seed.tier(2))
+            s4(Wheat.tier(2))
+            s6(Wheat.tier(2))
+            item { player, b ->
+                val newItem = HealingStrawHat(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    leatherColor = Color.YELLOW
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
