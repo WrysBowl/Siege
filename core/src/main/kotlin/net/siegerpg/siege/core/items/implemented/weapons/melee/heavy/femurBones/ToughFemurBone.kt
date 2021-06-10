450cdac195cc52ce8757b5ab58d1ee2037f5bcd7
Wryz <57204327+Wryz@users.noreply.github.com>
2021-06-09 21:52:35 -0400

    Femur Bones

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/weapons/melee/heavy/femurBones/ToughFemurBone.kt
@@ -0,0 +1,47 @@
+package net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.femurBones
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.weapons.CustomMeleeWeapon
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class ToughFemurBone() : CustomMeleeWeapon(
+    name = "Tough Femur Bone",
+    customModelData = 130003,
+    description = listOf("A large animal's femur"),
+    levelRequirement = 11,
+    material = Material.WOODEN_AXE,
+    baseStats = CustomItemUtils.statMap(strength = 20.0, toughness = 50.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Bone.tier(3))
+            s1(Bone.tier(3))
+            s1(Pebble.tier(3))
+            item { player, b ->
+                val newItem = ToughFemurBone(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    attackSpeed = 0.9
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
