b25d0f974c6fe2324de75bf75f5c74fe969603ac
Emily <lizemty36@gmail.com>
2021-06-13 16:48:32 -0400

    Wool (Leggings + Boots), Magma (Helmets + Chestplates)

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/leggings/woolLeggings/LuckyWoolLeggings.kt
@@ -0,0 +1,53 @@
+package net.siegerpg.siege.core.items.implemented.armor.leggings.woolLeggings
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
+import net.siegerpg.siege.core.items.types.armor.CustomLeggings
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class LuckyWoolLeggings() : CustomLeggings(
+    name = "Lucky Wool Leggings",
+    customModelData = 1,
+    description = listOf("Dressed for the winter"),
+    levelRequirement = 9,
+    material = Material.LEATHER_LEGGINGS,
+    baseStats = CustomItemUtils.statMap(health = 8.0, luck = 5.0, regeneration = 5.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Wool.tier(2))
+            s2(Wool.tier(2))
+            s3(Wool.tier(2))
+            s4(Feather.tier(2))
+            s6(Feather.tier(2))
+            s7(Feather.tier(2))
+            s9(Feather.tier(2))
+            item { player, b ->
+                val newItem = LuckyWoolLeggings(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    leatherColor = Color.WHITE
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/leggings/woolLeggings/StrongWoolLeggings.kt
@@ -0,0 +1,52 @@
+package net.siegerpg.siege.core.items.implemented.armor.leggings.woolLeggings
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
+import net.siegerpg.siege.core.items.types.armor.CustomLeggings
+import net.siegerpg.siege.core.utils.Utils
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class StrongWoolLeggings() : CustomLeggings(
+    name = "Strong Wool Leggings",
+    customModelData = 1,
+    description = listOf("Dressed for the winter"),
+    levelRequirement = 9,
+    material = Material.LEATHER_LEGGINGS,
+    baseStats = CustomItemUtils.statMap(health = 5.0, strength = 6.0),
+    recipeList = recipes {
+        recipe {
+            shaped = true
+            s1(Wool.tier(2))
+            s2(Wool.tier(2))
+            s3(Wool.tier(2))
+            s4(Bone.tier(2))
+            s6(Bone.tier(2))
+            s7(Bone.tier(2))
+            item { player, b ->
+                val newItem = StrongWoolLeggings(if (b) 50 else Utils.randRarity())
+                newItem.updateMeta(b)
+                newItem
+            }
+        }
+    },
+    leatherColor = Color.WHITE
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
