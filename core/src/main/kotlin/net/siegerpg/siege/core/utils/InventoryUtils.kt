66cd02fe78ef5c06c1fef7b35e3855aee3c7b4e5
Ghqst <legobikes@outlook.com>
2021-06-22 13:20:03 -0400

    work on shops

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/utils/InventoryUtils.kt
@@ -0,0 +1,16 @@
+package net.siegerpg.siege.core.utils
+
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.event.inventory.InventoryClickEvent
+
+val InventoryClickEvent.isMiddleClick: Boolean
+    get() = this.click == ClickType.MIDDLE
+
+val InventoryClickEvent.isDropClick: Boolean
+    get() = this.click == ClickType.DROP
+
+val InventoryClickEvent.isDropStackClick: Boolean
+    get() = this.click == ClickType.CONTROL_DROP
+
+val InventoryClickEvent.isOffhandKey: Boolean
+    get() = this.click == ClickType.SWAP_OFFHAND
