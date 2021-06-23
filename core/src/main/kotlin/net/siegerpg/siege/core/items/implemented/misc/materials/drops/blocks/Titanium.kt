24a2d5867ba3b27b4cd3706ca64d2ae84e1f8d7a
Wryz <57204327+Wryz@users.noreply.github.com>
2021-06-23 14:51:29 -0400

    Added titanium

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/materials/drops/blocks/Titanium.kt
@@ -0,0 +1,38 @@
+package net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks
+
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.recipes.recipes
+import net.siegerpg.siege.core.items.types.misc.CustomMaterial
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class Titanium() : CustomMaterial(
+    name = "Titanium",
+    customModelData = 320010,
+    description = listOf("Even better metal"),
+    levelRequirement = 0,
+    material = Material.FLINT,
+    recipeList = recipes {
+    }
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = quality
+        this.rarity = Rarity.getFromInt(quality)
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+    companion object {
+        fun tier(tier: Int): Titanium {
+            val newItem = Titanium(0)
+            newItem.tier = tier
+            return newItem
+        }
+    }
+
+}
