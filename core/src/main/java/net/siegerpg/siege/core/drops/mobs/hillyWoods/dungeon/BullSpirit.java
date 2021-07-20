5b3766f44e97bacbaf8f22a37c463ca5f016c3d2
Wryz <57204327+Wryz@users.noreply.github.com>
2021-07-20 19:24:13 -0400

    Bull Spirit Boss Drops

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/BullSpirit.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/BullSpirit.java
@@ -2,19 +2,31 @@ package net.siegerpg.siege.core.drops.mobs.hillyWoods.dungeon;
 
 import net.siegerpg.siege.core.drops.MobDropTable;
 import net.siegerpg.siege.core.drops.Reward;
-import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Pebble;
-import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Seed;
-import net.siegerpg.siege.core.items.implemented.misc.wands.RockWand;
-import net.siegerpg.siege.core.items.implemented.weapons.ranged.PebbleShooter;
+import net.siegerpg.siege.core.items.implemented.armor.boots.*;
+import net.siegerpg.siege.core.items.implemented.armor.chestplate.*;
+import net.siegerpg.siege.core.items.implemented.armor.helmet.*;
+import net.siegerpg.siege.core.items.implemented.armor.leggings.*;
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*;
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*;
+import net.siegerpg.siege.core.items.implemented.misc.wands.*;
+import net.siegerpg.siege.core.items.implemented.weapons.ranged.*;
 import net.siegerpg.siege.core.utils.Utils;
 
 public class BullSpirit extends MobDropTable {
     public BullSpirit() {
-        super("SlimeSpirit", 180, 200, 250, 270, new Reward[]{
-                new Reward(Pebble.Companion.tier(3).getUpdatedItem(false), 100.0),
-                new Reward(Seed.Companion.tier(3).getUpdatedItem(false), 100.0),
-                new Reward(new PebbleShooter(Utils.randRarity()).getUpdatedItem(false), 50.0),
-                new Reward(new RockWand(Utils.randRarity()).getUpdatedItem(false), 60.0)
+        super("SlimeSpirit", 700, 800, 800, 900, new Reward[]{
+                new Reward(Leather.Companion.tier(3).getUpdatedItem(false), 40.0),
+                new Reward(Leather.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 50.0),
+                new Reward(Leather.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 50.0),
+                new Reward(Leather.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
+                new Reward(Leather.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
+                new Reward(Leather.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
+                new Reward(Bone.Companion.tier(2).getUpdatedItem(false).asQuantity(16), 50.0),
+                new Reward(Bone.Companion.tier(2).getUpdatedItem(false).asQuantity(16), 50.0),
+                new Reward(new LeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
+                new Reward(new LeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
+                new Reward(new LeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
+                new Reward(new LeatherBoots(Utils.randRarity()).getUpdatedItem(false), 30.0),
         });
     }
 }
--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/SlimeSpirit.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/SlimeSpirit.java
@@ -2,6 +2,10 @@ package net.siegerpg.siege.core.drops.mobs.hillyWoods.dungeon;
 
 import net.siegerpg.siege.core.drops.MobDropTable;
 import net.siegerpg.siege.core.drops.Reward;
+import net.siegerpg.siege.core.items.implemented.armor.boots.*;
+import net.siegerpg.siege.core.items.implemented.armor.chestplate.*;
+import net.siegerpg.siege.core.items.implemented.armor.helmet.*;
+import net.siegerpg.siege.core.items.implemented.armor.leggings.*;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*;
 import net.siegerpg.siege.core.items.implemented.misc.wands.*;
@@ -18,7 +22,10 @@ public class SlimeSpirit extends MobDropTable {
                 new Reward(Slime.Companion.tier(2).getUpdatedItem(false).asQuantity(4), 30.0),
                 new Reward(Seed.Companion.tier(3).getUpdatedItem(false), 50.0),
                 new Reward(new SlimeSpoofer(Utils.randRarity()).getUpdatedItem(false), 50.0),
-                new Reward(new SlimeSpoofer(Utils.randRarity()).getUpdatedItem(false), 25.0)
+                new Reward(new SlimyHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
+                new Reward(new SlimyChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
+                new Reward(new SlimyLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
+                new Reward(new SlimyBoots(Utils.randRarity()).getUpdatedItem(false), 30.0)
         });
     }
 }
