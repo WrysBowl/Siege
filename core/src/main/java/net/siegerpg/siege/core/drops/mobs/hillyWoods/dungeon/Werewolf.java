32dcf8ac7b8d2d8ef06148eeaff147f7ef7d5c29
Wryz <57204327+Wryz@users.noreply.github.com>
2021-07-20 19:03:27 -0400

    Magma Spirit Boss Drops

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/Werewolf.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/Werewolf.java
@@ -2,19 +2,22 @@ package net.siegerpg.siege.core.drops.mobs.hillyWoods.dungeon;
 
 import net.siegerpg.siege.core.drops.MobDropTable;
 import net.siegerpg.siege.core.drops.Reward;
-import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Pebble;
-import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Seed;
-import net.siegerpg.siege.core.items.implemented.misc.wands.RockWand;
-import net.siegerpg.siege.core.items.implemented.weapons.ranged.PebbleShooter;
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*;
+import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*;
+import net.siegerpg.siege.core.items.implemented.misc.wands.*;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.*;
+import net.siegerpg.siege.core.items.implemented.weapons.ranged.*;
 import net.siegerpg.siege.core.utils.Utils;
 
 public class Werewolf extends MobDropTable {
     public Werewolf() {
-        super("SlimeSpirit", 180, 200, 250, 270, new Reward[]{
-                new Reward(Pebble.Companion.tier(3).getUpdatedItem(false), 100.0),
-                new Reward(Seed.Companion.tier(3).getUpdatedItem(false), 100.0),
-                new Reward(new PebbleShooter(Utils.randRarity()).getUpdatedItem(false), 50.0),
-                new Reward(new RockWand(Utils.randRarity()).getUpdatedItem(false), 60.0)
+        super("Werewolf", 400, 500, 500, 600, new Reward[]{
+                new Reward(Bone.Companion.tier(3).getUpdatedItem(false), 50.0),
+                new Reward(Bone.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 40.0),
+                new Reward(Bone.Companion.tier(1).getUpdatedItem(false).asQuantity(16), 30.0),
+                new Reward(Bone.Companion.tier(1).getUpdatedItem(false).asQuantity(16), 30.0),
+                new Reward(new FemurBone(Utils.randRarity()).getUpdatedItem(false), 50.0),
+                new Reward(new FemurBone(Utils.randRarity()).getUpdatedItem(false), 25.0),
         });
     }
 }
