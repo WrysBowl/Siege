3b8955eca6c103ee669cef9c78a499fe91e1d0ea
Wryz <57204327+Wryz@users.noreply.github.com>
2021-08-08 22:21:21 -0400

    Bee Pants onHit Method

--- core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/leggings/BeePants.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/armor/leggings/BeePants.kt
@@ -1,15 +1,18 @@
 package net.siegerpg.siege.core.items.implemented.armor.leggings
 
 import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.CustomItemUtils.getCustomItem
 import net.siegerpg.siege.core.items.enums.Rarity
-import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.Seed
-import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Magma
-import net.siegerpg.siege.core.items.recipes.recipes
 import net.siegerpg.siege.core.items.types.armor.CustomLeggings
-import net.siegerpg.siege.core.utils.Utils
 import org.bukkit.Color
 import org.bukkit.Material
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 class BeePants() : CustomLeggings(
     name = "Bee Stinger",
@@ -30,6 +33,18 @@ class BeePants() : CustomLeggings(
     constructor(item: ItemStack): this() {
         this.item = item
         deserialize()
+    }
+
+    override fun onHit(e: EntityDamageEvent) {
+        if (e !is EntityDamageByEntityEvent) return
+        if (e.damager is Player && e.damager.world.name == "PVP") return
+        val player = (e.damager as Player).player ?: return
+        val item = player.inventory.leggings
+        val cusItem = getCustomItem(item) ?: return
+        if (cusItem.levelRequirement == null) return
+        if (cusItem.levelRequirement!! > player.level) return
+        val potion = PotionEffect(PotionEffectType.POISON, 60, 3)
+        (e.entity as LivingEntity).addPotionEffect(potion)
     }
 
 }
