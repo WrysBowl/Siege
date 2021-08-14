55162b79554450fbedc1dcb03d77733ba2433c61
Wryz <57204327+Wryz@users.noreply.github.com>
2021-08-14 12:50:25 -0400

    Fish Lore Size Searching
    The issue never happens to me so I have no idea how to test it. It happens randomly to other people

--- core/src/main/java/net/siegerpg/siege/core/listeners/NPC/Henry.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/NPC/Henry.java
@@ -1,11 +1,10 @@
 package net.siegerpg.siege.core.listeners.NPC;
 
-import com.github.stefvanschie.inventoryframework.gui.GuiItem;
-import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
-import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
-import de.tr7zw.nbtapi.NBTContainer;
 import de.tr7zw.nbtapi.NBTItem;
-import kotlin.Suppress;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.siegerpg.siege.core.Core;
 import net.siegerpg.siege.core.fishing.fish.Fish;
 import net.siegerpg.siege.core.fishing.fish.FishCore;
@@ -20,6 +19,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Henry implements Listener {
 
@@ -31,7 +34,7 @@ public class Henry implements Listener {
         }
     }
     private void buyFish(Player player) {
-        ItemStack hand = player.getInventory().getItemInMainHand().asOne();
+        ItemStack hand = player.getInventory().getItemInMainHand();
         if (hand.getType().equals(Material.AIR)) return;
         NBTItem nbt = new NBTItem(hand);
         String fishName = nbt.getString("Name");
@@ -40,12 +43,28 @@ public class Henry implements Listener {
             return;
         }
         Fish fish = FishCore.getFish(fishName);
+        int goldAmount;
         if (fish == null) {
-            player.sendMessage(Utils.parse("<red>This is not a fish!"));
-            return;
+            List<Component> lore = hand.lore();
+            if (lore != null && lore.get(1).contains(Utils.lore("Size "))) {
+                Component lineOne = lore.get(1);
+                String line = PlainTextComponentSerializer.plainText().serialize(lineOne);
+                String newLine = line.replace("Size ", "").replace(" cm", "");
+                try {
+                    goldAmount = Integer.parseInt(newLine);
+                } catch(Exception ignored){
+                    player.sendMessage(Utils.parse("<red>This is not a fish!"));
+                    return;
+                }
+
+            } else {
+                player.sendMessage(Utils.parse("<red>This is not a fish!"));
+                return;
+            }
+        } else {
+            goldAmount = (int)fish.actualSize;
         }
 
-        int goldAmount = (int)fish.actualSize;
         VaultHook.econ.depositPlayer(player, goldAmount);
         player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
         player.sendActionBar(Utils.parse("<yellow>+ " + goldAmount + " <yellow>Gold"));
@@ -56,6 +75,6 @@ public class Henry implements Listener {
             }
         }, 20);
 
-        player.getInventory().removeItem(hand);
+        player.getInventory().removeItem(hand.asOne());
     }
 }
