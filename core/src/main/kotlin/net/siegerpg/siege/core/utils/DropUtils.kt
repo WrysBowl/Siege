5c504afae9378e4bd5d57cb6cc8f8bdf38466a9e
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-09-17 17:39:03 +0200

    added comments to my code

--- core/src/main/kotlin/net/siegerpg/siege/core/utils/DropUtils.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/utils/DropUtils.kt
@@ -16,8 +16,14 @@ import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.inventory.ItemStack
 import java.util.*
 
+/**
+ * Used to drop items only for certain players
+ */
 class DropUtils : Listener, PacketListenerAbstract() {
 
+    /**
+     * Disables item picking up for items you shouldn't be able to pick up.
+     */
     @EventHandler
     fun onItemPickup(evt: EntityPickupItemEvent) {
         val item = evt.item.itemStack
@@ -30,6 +36,9 @@ class DropUtils : Listener, PacketListenerAbstract() {
             evt.isCancelled = true
     }
 
+    /**
+     * Doesn't send the entity spawn packet for those entities
+     */
     override fun onPacketPlaySend(evt: PacketPlaySendEvent) {
         if (evt.packetId == PacketType.Play.Server.SPAWN_ENTITY_SPAWN) {
             val wrappedPacket = WrappedPacketOutSpawnEntity(evt.nmsPacket)
@@ -46,8 +55,35 @@ class DropUtils : Listener, PacketListenerAbstract() {
 
 
     companion object {
-        fun dropItemForPlayers(loc: Location, item: ItemStack, players: List<UUID>): Item {
-            return loc.world.dropItem(loc, item.setNbtTags(Pair("seepickableby", players.map { it::toString })))
+
+        /**
+         * Internal method used to set the nbt tags of an item
+         */
+        private fun itemWithSeepickableNbtTags(item: ItemStack, players: List<UUID>): ItemStack {
+            return item.setNbtTags(Pair("seepickableby", players.map { it::toString }))
+        }
+
+        /**
+         * Drops an item which will only be visible and pickable by specific players
+         * @param loc The location to drop the item at
+         * @param item The item to drop
+         * @param players The list of player uuids that will be able to pick the item up
+         */
+        public fun dropItemForPlayers(loc: Location, item: ItemStack, players: List<UUID>): Item {
+            return loc.world.dropItem(loc, itemWithSeepickableNbtTags(item, players))
+        }
+
+        /**
+         * Drops an item naturally which will only be visible and pickable by specific players
+         * @param loc The location to drop the item at
+         * @param item The item to drop
+         * @param players The list of player uuids that will be able to pick the item up
+         */
+        public fun dropItemNaturallyForPlayers(loc: Location, item: ItemStack, players: List<UUID>): Item {
+            return loc.world.dropItemNaturally(
+                loc,
+                itemWithSeepickableNbtTags(item, players)
+            )
         }
     }
 }
