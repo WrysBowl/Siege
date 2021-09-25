1c9d93eab2ddba41d845adf4b7d3eab40b15ac78
Wryz <57204327+Wryz@users.noreply.github.com>
2021-09-25 02:47:43 -0400

    Implemented Bull Spirit Drop Table

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/BullSpirit.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/BullSpirit.java
@@ -6,26 +6,44 @@ import net.siegerpg.siege.core.items.implemented.armor.boots.*;
 import net.siegerpg.siege.core.items.implemented.armor.chestplate.*;
 import net.siegerpg.siege.core.items.implemented.armor.helmet.*;
 import net.siegerpg.siege.core.items.implemented.armor.leggings.*;
+import net.siegerpg.siege.core.items.implemented.misc.food.Beef;
 import net.siegerpg.siege.core.items.implemented.misc.keys.hillyWoods.BullSpiritKey;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*;
+import net.siegerpg.siege.core.items.implemented.misc.statgems.healthGems.SimpleHealthGem;
+import net.siegerpg.siege.core.items.implemented.misc.statgems.toughGems.SimpleToughGem;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.DoubleBladedAxe;
 import net.siegerpg.siege.core.utils.Utils;
 
 public class BullSpirit extends MobDropTable {
     public BullSpirit() {
-        super("BullSpirit", 2600, 2700, 1850, 1900, new Reward[]{
-                new Reward(Leather.Companion.tier(3).getUpdatedItem(false), 40.0),
-                new Reward(Leather.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 50.0),
-                new Reward(Leather.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 50.0),
-                new Reward(Leather.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
-                new Reward(Leather.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
-                new Reward(Leather.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
-                new Reward(Bone.Companion.tier(2).getUpdatedItem(false).asQuantity(16), 50.0),
-                new Reward(Bone.Companion.tier(2).getUpdatedItem(false).asQuantity(16), 50.0),
-                new Reward(new LeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new LeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new LeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new LeatherBoots(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new BullSpiritKey(0).getUpdatedItem(false), 10.0)
+        super("BullSpirit", 37, 40, 46, 49, new Reward[]{
+                new Reward(Leather.Companion.tier(2).getUpdatedItem(false).asQuantity(4), 80.0),
+                new Reward(Leather.Companion.tier(3).getUpdatedItem(false).asQuantity(2), 8.0),
+                new Reward(Bone.Companion.tier(2).getUpdatedItem(false).asQuantity(4), 50.0),
+                new Reward(Bone.Companion.tier(3).getUpdatedItem(false).asQuantity(2), 25.0),
+
+                new Reward(new LeatherHelmet(100).getUpdatedItem(false), 0.5),
+                new Reward(new LeatherChestplate(100).getUpdatedItem(false), 0.5),
+                new Reward(new LeatherLeggings(100).getUpdatedItem(false), 0.5),
+                new Reward(new LeatherBoots(100).getUpdatedItem(false), 0.5),
+
+                new Reward(new LeatherHelmet(80).getUpdatedItem(false), 2.5),
+                new Reward(new LeatherChestplate(80).getUpdatedItem(false), 2.5),
+                new Reward(new LeatherLeggings(80).getUpdatedItem(false), 2.5),
+                new Reward(new LeatherBoots(80).getUpdatedItem(false), 2.5),
+
+                new Reward(new LeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 3.0),
+                new Reward(new LeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 3.0),
+                new Reward(new LeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 3.0),
+                new Reward(new LeatherBoots(Utils.randRarity()).getUpdatedItem(false), 3.0),
+
+                new Reward(new DoubleBladedAxe(100).getUpdatedItem(false), 0.5),
+                new Reward(new DoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 2.5),
+
+                new Reward(new Beef(100).getUpdatedItem(false), 20.0),
+                new Reward(new SimpleToughGem(0).getUpdatedItem(false), 0.25),
+                new Reward(new SimpleHealthGem(0).getUpdatedItem(false), 1.0),
+                new Reward(new BullSpiritKey(0).getUpdatedItem(false), 10.0),
 
         });
     }
