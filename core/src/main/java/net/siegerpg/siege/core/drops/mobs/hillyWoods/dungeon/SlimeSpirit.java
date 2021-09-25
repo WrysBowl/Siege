60df9ff6c19f1827e761308197204d26e8e65fb8
Wryz <57204327+Wryz@users.noreply.github.com>
2021-09-25 02:47:11 -0400

    Implemented Slime Spirit Drop Table

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/SlimeSpirit.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/SlimeSpirit.java
@@ -9,24 +9,39 @@ import net.siegerpg.siege.core.items.implemented.armor.leggings.*;
 import net.siegerpg.siege.core.items.implemented.misc.keys.hillyWoods.SlimeSpiritKey;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*;
+import net.siegerpg.siege.core.items.implemented.misc.statgems.luckGems.FlawedLuckGem;
+import net.siegerpg.siege.core.items.implemented.misc.statgems.regenerationGems.FlawedRegenerationGem;
 import net.siegerpg.siege.core.items.implemented.misc.wands.*;
 import net.siegerpg.siege.core.utils.Utils;
 
 public class SlimeSpirit extends MobDropTable {
     public SlimeSpirit() {
-        super("SlimeSpirit", 800, 900, 500, 600, new Reward[]{
-                new Reward(Slime.Companion.tier(1).getUpdatedItem(false).asQuantity(6), 25.0),
-                new Reward(Slime.Companion.tier(1).getUpdatedItem(false).asQuantity(6), 25.0),
-                new Reward(Slime.Companion.tier(1).getUpdatedItem(false).asQuantity(6), 25.0),
-                new Reward(Slime.Companion.tier(1).getUpdatedItem(false).asQuantity(6), 25.0),
-                new Reward(Slime.Companion.tier(2).getUpdatedItem(false).asQuantity(4), 30.0),
-                new Reward(Seed.Companion.tier(3).getUpdatedItem(false), 50.0),
-                new Reward(new SlimeSpoofer(Utils.randRarity()).getUpdatedItem(false), 50.0),
-                new Reward(new SlimyHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new SlimyChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new SlimyLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new SlimyBoots(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new SlimeSpiritKey(0).getUpdatedItem(false), 10.0)
+        super("SlimeSpirit", 340, 400, 340, 500, new Reward[]{
+                new Reward(Slime.Companion.tier(2).getUpdatedItem(false).asQuantity(4), 100.0),
+                new Reward(Slime.Companion.tier(3).getUpdatedItem(false).asQuantity(3), 30.0),
+                new Reward(Slime.Companion.tier(4).getUpdatedItem(false).asQuantity(2), 10.0),
+
+                new Reward(new SlimeSpoofer(100).getUpdatedItem(false), 10.0),
+                new Reward(new SlimeSpoofer(80).getUpdatedItem(false), 30.0),
+                new Reward(new SlimeSpoofer(Utils.randRarity()).getUpdatedItem(false), 5.0),
+
+                new Reward(new SlimyHelmet(100).getUpdatedItem(false), 10.0),
+                new Reward(new SlimyChestplate(100).getUpdatedItem(false), 10.0),
+                new Reward(new SlimyLeggings(100).getUpdatedItem(false), 10.0),
+                new Reward(new SlimyBoots(100).getUpdatedItem(false), 10.0),
+
+                new Reward(new SlimyHelmet(80).getUpdatedItem(false), 3.0),
+                new Reward(new SlimyChestplate(80).getUpdatedItem(false), 3.0),
+                new Reward(new SlimyLeggings(80).getUpdatedItem(false), 3.0),
+                new Reward(new SlimyBoots(80).getUpdatedItem(false), 3.0),
+
+                new Reward(new SlimyHelmet(Utils.randRarity()).getUpdatedItem(false), 15.0),
+                new Reward(new SlimyChestplate(Utils.randRarity()).getUpdatedItem(false), 15.0),
+                new Reward(new SlimyLeggings(Utils.randRarity()).getUpdatedItem(false), 15.0),
+                new Reward(new SlimyBoots(Utils.randRarity()).getUpdatedItem(false), 15.0),
+
+                new Reward(new FlawedRegenerationGem(0).getUpdatedItem(false), 10.0),
+                new Reward(new FlawedLuckGem(0).getUpdatedItem(false), 10.0),
         });
     }
 }
