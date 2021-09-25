104ec5fdd2c8490336b7b5e840e38e39fe484c09
Wryz <57204327+Wryz@users.noreply.github.com>
2021-09-25 02:47:18 -0400

    Implemented Magma Spirit Drop Table

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/MagmaSpirit.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/MagmaSpirit.java
@@ -9,23 +9,40 @@ import net.siegerpg.siege.core.items.implemented.armor.leggings.*;
 import net.siegerpg.siege.core.items.implemented.misc.keys.hillyWoods.MagmaSpiritKey;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.*;
+import net.siegerpg.siege.core.items.implemented.misc.statgems.regenerationGems.FlawedRegenerationGem;
+import net.siegerpg.siege.core.items.implemented.misc.statgems.strengthGems.FlawedStrengthGem;
+import net.siegerpg.siege.core.items.implemented.misc.wands.MatchStick;
+import net.siegerpg.siege.core.items.implemented.misc.wands.Torch;
 import net.siegerpg.siege.core.utils.Utils;
 
 public class MagmaSpirit extends MobDropTable {
     public MagmaSpirit() {
-        super("MagmaSpirit", 1250, 1300, 750, 850, new Reward[]{
-                new Reward(Magma.Companion.tier(3).getUpdatedItem(false), 40.0),
-                new Reward(Magma.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 50.0),
-                new Reward(Magma.Companion.tier(2).getUpdatedItem(false).asQuantity(8), 50.0),
-                new Reward(Magma.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
-                new Reward(Magma.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
-                new Reward(Magma.Companion.tier(1).getUpdatedItem(false).asQuantity(8), 25.0),
-                new Reward(Seed.Companion.tier(3).getUpdatedItem(false).asQuantity(16), 100.0),
-                new Reward(new MagmaHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new MagmaChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new MagmaLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new MagmaBoots(Utils.randRarity()).getUpdatedItem(false), 30.0),
-                new Reward(new MagmaSpiritKey(0).getUpdatedItem(false), 10.0)
+        super("MagmaSpirit", 540, 580, 620, 680, new Reward[]{
+                new Reward(Magma.Companion.tier(2).getUpdatedItem(false).asQuantity(4), 100.0),
+                new Reward(Magma.Companion.tier(3).getUpdatedItem(false).asQuantity(3), 30.0),
+                new Reward(Magma.Companion.tier(4).getUpdatedItem(false).asQuantity(2), 10.0),
+
+                new Reward(new MagmaHelmet(100).getUpdatedItem(false), 20.5),
+                new Reward(new MagmaChestplate(100).getUpdatedItem(false), 20.5),
+                new Reward(new MagmaLeggings(100).getUpdatedItem(false), 20.5),
+                new Reward(new MagmaBoots(100).getUpdatedItem(false), 20.5),
+
+                new Reward(new MagmaHelmet(80).getUpdatedItem(false), 10.5),
+                new Reward(new MagmaChestplate(80).getUpdatedItem(false), 10.5),
+                new Reward(new MagmaLeggings(80).getUpdatedItem(false), 10.5),
+                new Reward(new MagmaBoots(80).getUpdatedItem(false), 10.5),
+
+                new Reward(new MagmaHelmet(Utils.randRarity()).getUpdatedItem(false), 20.0),
+                new Reward(new MagmaChestplate(Utils.randRarity()).getUpdatedItem(false), 20.0),
+                new Reward(new MagmaLeggings(Utils.randRarity()).getUpdatedItem(false), 20.0),
+                new Reward(new MagmaBoots(Utils.randRarity()).getUpdatedItem(false), 20.0),
+
+                new Reward(new Torch(100).getUpdatedItem(false), 25.0),
+                new Reward(new Torch(Utils.randRarity()).getUpdatedItem(false), 25.0),
+
+                new Reward(new FlawedStrengthGem(0).getUpdatedItem(false), 20.0),
+                new Reward(new FlawedRegenerationGem(0).getUpdatedItem(false), 20.0),
+                new Reward(new MagmaSpiritKey(0).getUpdatedItem(false), 10.0),
         });
     }
 }
