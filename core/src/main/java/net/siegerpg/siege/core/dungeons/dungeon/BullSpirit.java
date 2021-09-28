fa05d71cd41065f346ef424d40cd7da07c877940
Wryz <57204327+Wryz@users.noreply.github.com>
2021-09-28 02:07:50 -0400

    Bull Spirit Implementation

---
+++ core/src/main/java/net/siegerpg/siege/core/dungeons/dungeon/BullSpirit.java
@@ -0,0 +1,34 @@
+package net.siegerpg.siege.core.dungeons.dungeon;
+
+import net.siegerpg.siege.core.Core;
+import net.siegerpg.siege.core.dungeons.Dungeon;
+import net.siegerpg.siege.core.items.implemented.misc.keys.hillyWoods.BullSpiritKey;
+import net.siegerpg.siege.core.items.implemented.misc.keys.hillyWoods.MagmaSpiritKey;
+import net.siegerpg.siege.core.utils.particleEffects.Helix;
+import net.siegerpg.siege.core.utils.particleEffects.Waves;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+
+public class BullSpirit extends Dungeon {
+
+    public BullSpirit() {
+        super("BullSpirit", 8,
+                new BullSpiritKey(0),
+                new Location(null, 272, 70, 20), "Hilly_Woods",
+                100
+        );
+    }
+
+    @Override
+    public void spawning() {
+        Location loc = new Location(Core.plugin().getServer().getWorld("Hilly_Woods"), 272, 70, 20);
+        new Helix().createHelix(loc, Particle.CRIT);
+
+        Bukkit.getServer().getScheduler().runTaskLater(Core.plugin(), () -> {
+            new Waves().createWaves(loc);
+            loc.getWorld().playSound(loc, Sound.ENTITY_WITHER_SPAWN, 1.0f, 1.0f);
+        }, 80);
+    }
+}
