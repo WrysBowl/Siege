774b48d3266d17b2cd73023773ac3a00ad1ab642
Wryz <57204327+Wryz@users.noreply.github.com>
2021-10-12 01:40:38 -0400

    Update Rainbow.kt

--- core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/cosmetics/legendary/Rainbow.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/cosmetics/legendary/Rainbow.kt
@@ -2,8 +2,10 @@ package net.siegerpg.siege.core.items.implemented.misc.cosmetics.legendary
 
 import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
 import net.siegerpg.siege.core.Core
+import net.siegerpg.siege.core.items.CustomItem
 import net.siegerpg.siege.core.items.CustomItemUtils
 import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
 import net.siegerpg.siege.core.items.types.misc.Cosmetic
 import net.siegerpg.siege.core.items.types.subtypes.CustomCosmetic
 import net.siegerpg.siege.core.listeners.ArmorEquip.ArmorEquipEvent
@@ -20,8 +22,9 @@ class Rainbow() : Cosmetic(
 
     override fun onCosmeticEquip(e: PlayerArmorChangeEvent) {
         val player = e.player
+        Bukkit.getLogger().info("1")
         val newArmor = CustomItemUtils.getCustomItem(e.newItem) ?: return
-        if (newArmor !is Rainbow) return
+        Bukkit.getLogger().info("2")
         val rainbowArray: ArrayList<Material> = arrayListOf(
             Material.RED_STAINED_GLASS,
             Material.ORANGE_STAINED_GLASS,
@@ -35,23 +38,21 @@ class Rainbow() : Cosmetic(
             Material.MAGENTA_STAINED_GLASS,
             Material.PURPLE_STAINED_GLASS
         )
+        var ticker = 0
         object : BukkitRunnable() {
-            var ticker = 0
             override fun run() {
-                ticker += 1
-                if (ticker >= rainbowArray.size) ticker = 0
-
-                //change the material type of the cosmetic helmet to the stained glass
-                //update meta
-                newArmor.material = rainbowArray[ticker]
-                newArmor.updateMeta(false)
-
-                if (player.inventory.helmet == null ||
-                    !rainbowArray.contains(player.inventory.helmet!!.type)) {
+                if (player.inventory.helmet == null) {
+                    Bukkit.getLogger().info("3")
                     cancel()
+                } else if (player.inventory.helmet == newArmor.item){
+                    ticker += 1
+                    if (ticker == rainbowArray.size) ticker = 0
+                    newArmor.material = rainbowArray[ticker]
+                    newArmor.updateMeta(false)
+                    player.inventory.helmet = newArmor.item
                 }
             }
-        }.runTaskTimer(Core.plugin(), 0, 5)
+        }.runTaskTimer(Core.plugin(), 0, 20)
     }
 
 
