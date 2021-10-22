4d25bcdee6b6efddd7827cc8cee8216e443793f3
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-10-22 16:53:53 +0200

    Updated shadowing plugin (otherwise issues)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.5.30"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 repositories {
