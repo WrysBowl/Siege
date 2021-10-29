8c7ce94b3505180c49b7c2316c91173a1bcaca7f
Wryz <57204327+Wryz@users.noreply.github.com>
2021-10-29 11:39:10 -0400

    Teleporting Scoreboard Update

--- core/src/main/java/net/siegerpg/siege/core/listeners/EntityTeleportListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/EntityTeleportListener.java
@@ -1,14 +1,14 @@
 package net.siegerpg.siege.core.listeners;
 
-import org.bukkit.Effect;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import net.siegerpg.siege.core.Core;
+import net.siegerpg.siege.core.utils.Scoreboard;
+import org.bukkit.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class EntityTeleportListener implements Listener {
 
@@ -22,5 +22,11 @@ public class EntityTeleportListener implements Listener {
                 ((Player)entity).playSound(entity.getLocation(), Sound.ENTITY_WITHER_SHOOT, 1.0f, 1.0f);
             }
         }
+    }
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent e) {
+        Bukkit.getServer().getScheduler().runTaskLater(Core.plugin(), () -> {
+            Scoreboard.updateScoreboard(e.getPlayer());
+        }, 2);
     }
 }
