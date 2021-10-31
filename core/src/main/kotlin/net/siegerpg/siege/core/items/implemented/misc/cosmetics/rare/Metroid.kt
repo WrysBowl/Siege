7b96d3dc9a8e0abe6d1d7be894192efe54a58485
Wryz <57204327+Wryz@users.noreply.github.com>
2021-10-30 21:25:57 -0400

    Create Metroid.kt

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/cosmetics/rare/Metroid.kt
@@ -0,0 +1,26 @@
+package net.siegerpg.siege.core.items.implemented.misc.cosmetics.rare
+
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.types.misc.Cosmetic
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class Metroid() : Cosmetic(
+    name = "Metroid",
+    customModelData = 730007,
+    description = listOf(""),
+    material = Material.KNOWLEDGE_BOOK,
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = 80
+        this.rarity = Rarity.RARE
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        deserialize()
+    }
+
+}
