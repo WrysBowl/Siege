1c4067cdfe5184c6fa95db2cfb3c7e7a44c125ac
DJ <93049591+Dj-Danger123@users.noreply.github.com>
2021-10-31 17:09:15 +0000

    Help command register

---
+++ core/src/main/java/net/siegerpg/siege/core/commands/Help.java
@@ -0,0 +1,22 @@
+package net.siegerpg.siege.core.commands;
+
+import net.siegerpg.siege.core.utils.Utils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class Help implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Player player = (Player) sender;
+        player.sendMessage(Utils.parse(""));
+        player.sendMessage(Utils.parse("  <aqua><bold>Join our discord here!<reset>"));
+        player.sendMessage(Utils.tacc("  https://discord.siegerpg.net"));
+        player.sendMessage(Utils.parse(""));
+        return false;
+    }
+}
