ec79263142a7a5f67794d7e06d106474226c1ee0
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-01 14:03:46 -0400

    Wheat Armor Tier 3->2 Cost

--- shops/src/main/kotlin/net/siegerpg/siege/shops/shop/Joe.kt
+++ shops/src/main/kotlin/net/siegerpg/siege/shops/shop/Joe.kt
@@ -24,7 +24,7 @@ class Joe : Shop() {
     override var items: List<ShopItem> = listOf(
         //STRAW HAT
         ShopItem(StrawHelmet(-1), 400, hashMapOf(
-            Wheat.tier(3) to 4), true) {
+            Wheat.tier(2) to 4), true) {
             StrawHelmet(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
@@ -32,37 +32,37 @@ class Joe : Shop() {
         },
         ShopItem(
             LuckyStrawHat(-1), 500, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Feather.tier(2) to 4), true) {
             LuckyStrawHat(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             StrongStrawHat(-1), 500, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Bone.tier(2) to 4), true) {
             StrongStrawHat(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             ToughStrawHat(-1), 500, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Pebble.tier(2) to 4), true) {
             ToughStrawHat(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealthyStrawHat(-1), 500, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 PlantMatter.tier(2) to 4), true) {
             HealthyStrawHat(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealingStrawHat(-1), 500, hashMapOf(
-                Wheat.tier(3) to 3), true) {
+                Wheat.tier(2) to 3), true) {
             HealingStrawHat(Utils.randRarity()).getUpdatedItem(false)
         },
 
         //STRAW CHESTPLATE
         ShopItem(StrawChestplate(-1), 600, hashMapOf(
-            Wheat.tier(3) to 8), true) {
+            Wheat.tier(2) to 8), true) {
             StrawChestplate(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
@@ -70,37 +70,37 @@ class Joe : Shop() {
         },
         ShopItem(
             LuckyStrawChestplate(-1), 700, hashMapOf(
-                Wheat.tier(3) to 6,
+                Wheat.tier(2) to 6,
                 Feather.tier(2) to 2), true) {
             LuckyStrawChestplate(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             StrongStrawChestplate(-1), 700, hashMapOf(
-                Wheat.tier(3) to 6,
+                Wheat.tier(2) to 6,
                 Bone.tier(2) to 2), true) {
             StrongStrawChestplate(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             ToughStrawChestplate(-1), 700, hashMapOf(
-                Wheat.tier(3) to 6,
+                Wheat.tier(2) to 6,
                 Pebble.tier(2) to 5), true) {
             ToughStrawChestplate(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealthyStrawChestplate(-1), 700, hashMapOf(
-                Wheat.tier(3) to 6,
+                Wheat.tier(2) to 6,
                 PlantMatter.tier(2) to 4), true) {
             HealthyStrawChestplate(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealingStrawChestplate(-1), 700, hashMapOf(
-                Wheat.tier(3) to 10), true) {
+                Wheat.tier(2) to 10), true) {
             HealingStrawChestplate(Utils.randRarity()).getUpdatedItem(false)
         },
 
         //STRAW LEGGINGS
         ShopItem(StrawLeggings(-1), 500, hashMapOf(
-            Wheat.tier(3) to 7), true) {
+            Wheat.tier(2) to 7), true) {
             StrawLeggings(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
@@ -108,37 +108,37 @@ class Joe : Shop() {
         },
         ShopItem(
             LuckyStrawOveralls(-1), 600, hashMapOf(
-                Wheat.tier(3) to 3,
+                Wheat.tier(2) to 3,
                 Feather.tier(2) to 3), true) {
             LuckyStrawOveralls(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             StrongStrawOveralls(-1), 600, hashMapOf(
-                Wheat.tier(3) to 4,
+                Wheat.tier(2) to 4,
                 Bone.tier(2) to 3), true) {
             StrongStrawOveralls(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             ToughStrawOveralls(-1), 600, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Pebble.tier(2) to 5), true) {
             ToughStrawOveralls(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealthyStrawOveralls(-1), 600, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 PlantMatter.tier(2) to 5), true) {
             HealthyStrawOveralls(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealingStrawOveralls(-1), 600, hashMapOf(
-                Wheat.tier(3) to 8), true) {
+                Wheat.tier(2) to 8), true) {
             HealingStrawOveralls(Utils.randRarity()).getUpdatedItem(false)
         },
 
         //STRAW BOOTS
         ShopItem(StrawBoots(-1), 300, hashMapOf(
-            Wheat.tier(3) to 4), true) {
+            Wheat.tier(2) to 4), true) {
             StrawBoots(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
@@ -146,31 +146,31 @@ class Joe : Shop() {
         },
         ShopItem(
             LuckyStrawBoots(-1), 400, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Feather.tier(2) to 2), true) {
             LuckyStrawBoots(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             StrongStrawBoots(-1), 400, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Bone.tier(2) to 2), true) {
             StrongStrawBoots(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             ToughStrawBoots(-1), 400, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 Pebble.tier(2) to 3), true) {
             ToughStrawBoots(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealthyStrawBoots(-1), 400, hashMapOf(
-                Wheat.tier(3) to 2,
+                Wheat.tier(2) to 2,
                 PlantMatter.tier(2) to 3), true) {
             HealthyStrawBoots(Utils.randRarity()).getUpdatedItem(false)
         },
         ShopItem(
             HealingStrawBoots(-1), 400, hashMapOf(
-                Wheat.tier(3) to 5), true) {
+                Wheat.tier(2) to 5), true) {
             HealingStrawBoots(Utils.randRarity()).getUpdatedItem(false)
         },
     )
