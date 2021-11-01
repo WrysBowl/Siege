ebe980da6b0ec9d2cebd34b045c0d414ae5ea3be
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-01 11:04:16 -0400

    Introduction Webstore Commands Listener

--- core/src/main/java/net/siegerpg/siege/core/webstore/WebstoreListener.java
+++ core/src/main/java/net/siegerpg/siege/core/webstore/WebstoreListener.java
@@ -4,19 +4,30 @@ import de.tr7zw.nbtapi.NBTItem;
 import net.siegerpg.siege.core.Core;
 import net.siegerpg.siege.core.utils.Scoreboard;
 import net.siegerpg.siege.core.utils.Utils;
+import net.siegerpg.siege.core.utils.WebstoreDB;
 import net.siegerpg.siege.core.utils.cache.GlobalMultipliers;
 import net.siegerpg.siege.core.webstore.categories.boosters.WebstoreBoosters;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class WebstoreListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        String[] commands = WebstoreDB.INSTANCE.blockingGetStoreCommands(e.getPlayer());
+
+        //TODO: Iterate through the array and convert each iteration from string to array
+        //  , separated by a space. Then add the uuid of the player to the 0th index
+    }
 
     @EventHandler
     public void onEXPBoosterRedeem(PlayerInteractEvent e) {
