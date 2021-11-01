d76c04e98cae42fa9cd60a0895fa953dda66a83d
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-01 14:27:43 -0400

    Implemented New Foods
    Added watermelon, pumpkin pie, and carrot

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/food/PumpkinPie.kt
@@ -0,0 +1,30 @@
+package net.siegerpg.siege.core.items.implemented.misc.food
+
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.types.misc.CustomFood
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+
+class PumpkinPie() : CustomFood(
+    name = "Pumpkin Pie",
+    customModelData = 330011,
+    description = listOf("The best pie"),
+    levelRequirement = 0,
+    material = Material.PUMPKIN_PIE,
+    health = 20.0,
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = 0
+        this.rarity = Rarity.COMMON
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        this.deserialize()
+    }
+
+}
---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/food/Watermelon.kt
@@ -0,0 +1,33 @@
+package net.siegerpg.siege.core.items.implemented.misc.food
+
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.types.misc.CustomFood
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+
+class Watermelon() : CustomFood(
+    name = "Watermelon",
+    customModelData = 330010,
+    description = listOf(
+        "A large oblong fruit with",
+        "a hard green or white rind",
+        "often striped or variegated"),
+    levelRequirement = 0,
+    material = Material.MELON_SLICE,
+    health = 10.0,
+) {
+
+    constructor(quality: Int): this() {
+        this.quality = 0
+        this.rarity = Rarity.COMMON
+        this.serialize()
+    }
+
+    constructor(item: ItemStack): this() {
+        this.item = item
+        this.deserialize()
+    }
+
+}
