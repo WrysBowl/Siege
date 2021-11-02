649ef0aeed964d7f8b3209ca6987042060004360
DevMystical <66150056+DevMystical@users.noreply.github.com>
2021-11-02 16:57:32 -0400

    Spongebob & Noteworthy Cosmetics
    Added functionality to Spongebob and Noteworthy cosmetic items

--- core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/cosmetics/epic/Spongebob.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/cosmetics/epic/Spongebob.kt
@@ -1,16 +1,36 @@
 package net.siegerpg.siege.core.items.implemented.misc.cosmetics.epic
 
+import net.siegerpg.siege.core.Core
 import net.siegerpg.siege.core.items.enums.Rarity
 import net.siegerpg.siege.core.items.types.misc.Cosmetic
 import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.scheduler.BukkitRunnable
 
 class Spongebob() : Cosmetic(
     name = "Spongebob",
     customModelData = 1,
-    description = listOf(""),
+    description = listOf("Who lives in a pineapple under the sea..."),
     material = Material.SPONGE,
 ) {
+    override fun onCosmeticDamage(e: EntityDamageByEntityEvent) {
+        val player = e.entity as Player
+        val playerHelmet = player.inventory.helmet ?: return
+        playerHelmet.type = Material.WET_SPONGE
+
+        object : BukkitRunnable() {
+            override fun run() {
+                if (player.inventory.helmet == null || !(e.entity as Player).isOnline) {
+                    cancel()
+                } else if (player.inventory.helmet == playerHelmet) {
+                    playerHelmet.type = Material.SPONGE;
+                }
+            }
+        }.runTaskLater(Core.plugin(), 40);
+    }
+
     constructor(quality: Int): this() {
         this.quality = 90
         this.rarity = Rarity.EPIC
