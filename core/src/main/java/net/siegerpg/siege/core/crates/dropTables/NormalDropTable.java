aea3496e4da3e0a5c7a67055211b40adac23824f
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-02 15:24:58 -0400

    Drop Table Normal/Superior/Spirit

--- core/src/main/java/net/siegerpg/siege/core/crates/dropTables/NormalDropTable.java
+++ core/src/main/java/net/siegerpg/siege/core/crates/dropTables/NormalDropTable.java
@@ -2,7 +2,8 @@ package net.siegerpg.siege.core.crates.dropTables;
 
 
 import net.siegerpg.siege.core.crates.CosmeticDropTable;
-import net.siegerpg.siege.core.items.implemented.misc.cosmetics.common.AnonMask;
+import net.siegerpg.siege.core.items.implemented.misc.cosmetics.common.*;
+import net.siegerpg.siege.core.items.implemented.misc.cosmetics.uncommon.*;
 
 import java.util.HashMap;
 
@@ -10,7 +11,30 @@ public class NormalDropTable extends CosmeticDropTable {
     public NormalDropTable() {
         dropTable = new HashMap<>(){
             {
-                put(new AnonMask(0), 10);
+                //Common
+                put(new AnonMask(0), 20);
+                put(new ArrowHead(0), 20);
+                put(new BunnyEars(0), 20);
+                put(new Chocolate(0), 20);
+                put(new DogEars(0), 20);
+                put(new GrouchoGlasses(0), 20);
+                put(new Halo(0), 20);
+                put(new Mohawk(0), 20);
+                put(new PartyHat(0), 20);
+                put(new PigNose(0), 20);
+                put(new SquidGame(0), 20);
+                put(new StrawHat(0), 20);
+                put(new SurgicalMask(0), 20);
+
+                //Uncommon
+                put(new Bee(0), 10);
+                put(new CrusaderHelmet(0), 10);
+                put(new DavyJonesCap(0), 10);
+                put(new GooBall(0), 10);
+                put(new SlimeBlock(0), 10);
+                put(new StoneFace(0), 10);
+                put(new WiseGuy(0), 10);
+                put(new WitchHat(0), 10);
             }
         };
     }
--- core/src/main/java/net/siegerpg/siege/core/crates/dropTables/SpiritDropTable.java
+++ core/src/main/java/net/siegerpg/siege/core/crates/dropTables/SpiritDropTable.java
@@ -3,6 +3,8 @@ package net.siegerpg.siege.core.crates.dropTables;
 
 import net.siegerpg.siege.core.crates.CosmeticDropTable;
 import net.siegerpg.siege.core.items.implemented.misc.cosmetics.common.AnonMask;
+import net.siegerpg.siege.core.items.implemented.misc.cosmetics.epic.*;
+import net.siegerpg.siege.core.items.implemented.misc.cosmetics.rare.*;
 
 import java.util.HashMap;
 
@@ -10,7 +12,27 @@ public class SpiritDropTable extends CosmeticDropTable {
     public SpiritDropTable() {
         dropTable = new HashMap<>(){
             {
-                put(new AnonMask(0), 10);
+                //Rare
+                put(new Antlers(0), 20);
+                put(new AxeHead(0), 20);
+                put(new Axolotl(0), 20);
+                put(new Bedridden(0), 20);
+                put(new BlueBetaBlob(0), 20);
+                put(new Honey(0), 20);
+                put(new Metroid(0), 20);
+                put(new Panda(0), 20);
+                put(new Pyromancer(0), 20);
+                put(new TnT(0), 20);
+                put(new Unirod(0), 20);
+
+                //Epic
+                put(new CarrotOnAStick(0), 10);
+                put(new Crown(0), 10);
+                put(new HotHead(0), 10);
+                put(new Noteworthy(0), 10);
+                put(new RedHeadphones(0), 10);
+                put(new Spongebob(0), 10);
+                put(new SquidHead(0), 10);
             }
         };
     }
