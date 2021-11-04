29dbca484d65885b69f6a4eccb8f79e11463dccd
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-04 01:46:51 -0400

    New Level Up Sound Using Pitch Diff

--- core/src/main/kotlin/net/siegerpg/siege/core/utils/Levels.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/utils/Levels.kt
@@ -54,7 +54,8 @@ object Levels {
             object : BukkitRunnable() {
                 override fun run() {
                     reward.giveReward(player as Player, lvl)
-                    player.playSound(player.location, Sound.BLOCK_BEACON_POWER_SELECT, 1.0f, 1.0f)
+                    player.playSound(player.location, Sound.BLOCK_BEACON_POWER_SELECT, 1.0f, 2.0f)
+                    player.playSound(player.location, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 0.5f)
                 }
             }.runTaskLater(Core.plugin(), 20)
         }
