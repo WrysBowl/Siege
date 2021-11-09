bee434cd571d44fc13b50c03bd65733394083641
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-09 18:15:03 -0500

    Chat Text Formatting for Ranked

--- core/src/main/java/net/siegerpg/siege/core/listeners/ChatListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/ChatListener.java
@@ -27,30 +27,35 @@ public class ChatListener implements Listener {
         String prefix = net.siegerpg.siege.core.utils.VaultHook.perms.getPrimaryGroup(player);
         String message = e.getMessage().replaceAll("&k", "");
         String check = Utils.strip(message);
+
         if (check.equalsIgnoreCase("") || check.equalsIgnoreCase(" ")) {
             e.getPlayer().sendMessage(Utils.tacc("You can not send a empty message!"));
             e.setCancelled(true);
             return;
         }
+
         for (Player p : Bukkit.getOnlinePlayers()) {
             if (message.contains(p.getName())) {
                 p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 1.0F, 2.0F);
             }
         }
+
         if (message.contains("[item]")) {
             e.setCancelled(true);
             if (player.getInventory().getItemInMainHand().getType() != Material.AIR) {
                 ItemStack item = player.getInventory().getItemInMainHand();
                 String name = item.getItemMeta().getDisplayName();
                 if (name.equals("")) name = player.getInventory().getItemInMainHand().getI18NDisplayName();
-                Component miniMessage = Utils.lore("<bold><yellow>" + item.getAmount() + "x " + name).hoverEvent(item);
+                Component miniMessage = Utils.lore("<yellow>" + item.getAmount() + "x " + name).hoverEvent(item);
                 Component prefixes = Utils.lore(Utils.tacc(level + " " + prefix + " &7" + player.getName() + " &f"));
-                Component actualMessage = Utils.lore(e.getMessage()).replaceText("[item]", miniMessage);
+                Component actualMessage = Utils.lore(Utils.tacc(e.getMessage())).replaceText("[item]", miniMessage);
                 Bukkit.getServer().sendMessage(prefixes.append(actualMessage));
                 return;
             }
             player.sendMessage(Utils.lore("<red>You need an item to display!"));
         }
-        e.setFormat(Utils.tacc(level + " " + prefix + " ") + Utils.tacc("&7%1$s &f%2$s"));
+        if (player.hasPermission("siege.text.format")) {
+            e.setFormat(Utils.tacc(level + " " + prefix + " ") + Utils.tacc("&7%1$s &f"+message));
+        } else e.setFormat(Utils.tacc(level + " " + prefix + " ") + Utils.tacc("&7%1$s &f%2$s"));
     }
 }
