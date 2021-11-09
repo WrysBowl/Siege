98c6f2f45c5471b039eea89dd53c87d9e6c1ad99
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-09 02:11:26 -0500

    Level Up Title Message Fix

--- core/src/main/kotlin/net/siegerpg/siege/core/levelReward/Reward26.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/levelReward/Reward26.kt
@@ -10,7 +10,7 @@ class Reward26 : LevelReward {
     override fun giveReward(player: Player) {
         val level: Short = 27
         player.playSound(player.location, Sound.ENTITY_PLAYER_LEVELUP,5.0f, 5.0f)
-        player.sendTitle(Utils.tacc(""), Utils.tacc("&d${level-1} &7\u21E2 &5$level"), 10, 80, 10)
+        player.sendTitle(Utils.tacc("&5Level Up!"), Utils.tacc("&d${level-1} &7\u21E2 &5$level"), 10, 80, 10)
         player.sendMessage(Utils.lore(""))
         player.sendMessage(Utils.lore("<gray>You leveled up to level $level!"))
         player.sendMessage(Utils.lore("<gold><bold>Level Reward"))
--- core/src/main/kotlin/net/siegerpg/siege/core/levelReward/Reward29.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/levelReward/Reward29.kt
@@ -20,7 +20,7 @@ class Reward29 : LevelReward {
         Bukkit.getServer().broadcast(Utils.lore(""))
 
         player.playSound(player.location, Sound.ENTITY_PLAYER_LEVELUP, 5.0f, 5.0f)
-        player.sendTitle(Utils.tacc(""), Utils.tacc("&d${level-1} &7\u21E2 &5$level"), 10, 80, 10)
+        player.sendTitle(Utils.tacc("&5Level Up!"), Utils.tacc("&d${level-1} &7\u21E2 &5$level"), 10, 80, 10)
         player.sendMessage(Utils.lore(""))
         player.sendMessage(Utils.lore("<gray>You leveled up to level $level!"))
         player.sendMessage(Utils.lore("<gold><bold>Level Reward"))
