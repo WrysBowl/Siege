ce983e5c98e63d3b358a65088fa1d7e551a1dae7
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-09 16:04:43 -0500

    Buff Golden Goblin Gold Drop

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/hostile/GoldenGoblin.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/hostile/GoldenGoblin.java
@@ -12,12 +12,10 @@ import net.siegerpg.siege.core.utils.Utils;
 
 public class GoldenGoblin extends MobDropTable {
     public GoldenGoblin() {
-        super("GoldenGoblin", 100, 115, 60, 69, new Reward[]{
+        super("GoldenGoblin", 300, 400, 60, 69, new Reward[]{
                 new Reward(Leather.Companion.tier(2).getUpdatedItem(false), 50.0),
                 new Reward(Bone.Companion.tier(2).getUpdatedItem(false), 100.0),
                 new Reward(new Twig(Utils.randRarity()).getUpdatedItem(false), 100.0),
-                new Reward(new CrackedLuckGem(0).getUpdatedItem(false), 10.0),
-                new Reward(new RawLuckGem(0).getUpdatedItem(false), 20.0),
         });
     }
 }
