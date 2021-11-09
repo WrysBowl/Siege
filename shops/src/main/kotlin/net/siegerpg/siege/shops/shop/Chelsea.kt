99c0a871c0efd16932da4b1dc02d9fb34f1435d5
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-09 02:30:57 -0500

    Chelsea Golden Carrot & Product Changes

--- shops/src/main/kotlin/net/siegerpg/siege/shops/shop/Chelsea.kt
+++ shops/src/main/kotlin/net/siegerpg/siege/shops/shop/Chelsea.kt
@@ -1,8 +1,6 @@
 package net.siegerpg.siege.shops.shop
 
-import net.siegerpg.siege.core.items.implemented.misc.food.Apple
-import net.siegerpg.siege.core.items.implemented.misc.food.Bread
-import net.siegerpg.siege.core.items.implemented.misc.food.Drumstick
+import net.siegerpg.siege.core.items.implemented.misc.food.*
 import net.siegerpg.siege.core.items.implemented.misc.materials.*
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.*
 import net.siegerpg.siege.shops.*
@@ -14,69 +12,39 @@ class Chelsea : Shop() {
         ShopItem(Drumstick(0), 25, hashMapOf(), false) {
             Drumstick(0).getUpdatedItem(false)
         },
+        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },
         ShopItem(Apple(0), 20, hashMapOf(), false) {
             Apple(0).getUpdatedItem(false)
         },
-        ShopItem(Bread(0), 40, hashMapOf(), false) {
+        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },
+        ShopItem(Bread(0), 40, hashMapOf(Wheat.tier(2) to 4), false) {
             Bread(0).getUpdatedItem(false)
         },
-        ShopItem(Bread(0), -1, hashMapOf(Wheat.tier(2) to 4), true) {
+
+        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        }, ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
+            GRAYFILLER(-1).getUpdatedItem(false)
+        },
+        ShopItem(GoldenCarrot(0), 1000, hashMapOf(Carrot(0) to 512), false) {
             Bread(0).getUpdatedItem(false)
         },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
 
-        //Quality 50
-        ShopItem(Drumstick(50), 60, hashMapOf(), false) {
-            Drumstick(50).getUpdatedItem(false)
-        },
-        ShopItem(Apple(50), 45, hashMapOf(), false) {
-            Apple(50).getUpdatedItem(false)
-        },
-        ShopItem(Bread(50), 80, hashMapOf(), false) {
-            Bread(50).getUpdatedItem(false)
-        },
-        ShopItem(Bread(50), -1, hashMapOf(Wheat.tier(3) to 1), true) {
-            Bread(50).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
 
-        //Quality 1000
-        ShopItem(Drumstick(100), 110, hashMapOf(), false) {
-            Drumstick(100).getUpdatedItem(false)
-        },
-        ShopItem(Apple(100), 75, hashMapOf(), false) {
-            Apple(100).getUpdatedItem(false)
-        },
-        ShopItem(Bread(100), 140, hashMapOf(), false) {
-            Bread(100).getUpdatedItem(false)
-        },
-        ShopItem(Bread(100), -1, hashMapOf(Wheat.tier(3) to 3), true) {
-            Bread(100).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        },
-        ShopItem(GRAYFILLER(-1), -1, hashMapOf(), false) {
-            GRAYFILLER(-1).getUpdatedItem(false)
-        }
+
     )
 }
