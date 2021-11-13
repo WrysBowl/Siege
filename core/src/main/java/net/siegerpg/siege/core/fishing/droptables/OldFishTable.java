39fe80618be04fff8a6694f9367cb0b9178e13f4
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-13 14:14:34 -0500

    Nerfed Davy Drops

--- core/src/main/java/net/siegerpg/siege/core/fishing/droptables/OldFishTable.java
+++ core/src/main/java/net/siegerpg/siege/core/fishing/droptables/OldFishTable.java
@@ -8,16 +8,11 @@ public class OldFishTable extends FishDropTable {
     public OldFishTable() {
         this.fishDropTable = new HashMap<>(){
             {
-                put(new Bearacuda(), 15.0);
-                put(new BigBlueTuna(), 10.0);
-                put(new Catastrophe(), 60.0);
+                put(new Catastrophe(), 90.0);
                 put(new Codzilla(), 100.0);
-                put(new FlashyShark(), 5.0);
-                put(new MrKrabs(), 80.0);
-                put(new MrsPuff(), 60.0);
-                put(new PistolWhipper(), 50.0);
-                put(new RedSnacker(), 90.0);
-                put(new StingWhip(), 45.0);
+                put(new MrKrabs(), 40.0);
+                put(new MrsPuff(), 10.0);
+                put(new PistolWhipper(), 30.0);
             }
         };
     }
