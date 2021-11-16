49fdf1dff82e2184f9d639daa17bb8a8d3c886bd
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-16 17:40:29 -0500

    Damage Hologram Changes (for now)

--- core/src/main/kotlin/net/siegerpg/siege/core/miscellaneous/DamageIndicator.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/miscellaneous/DamageIndicator.kt
@@ -34,7 +34,8 @@ class DamageIndicator : Listener {
 	}
 
 	fun getIndicatorText(damage : Double, critical : Boolean) : String {
-		var formatter = when (damage) {
+		var formatter = if (critical) DecimalFormat("&6-0.#&4❤") else DecimalFormat("&c-0.#&4❤")
+		/*var formatter = when (damage) {
 			in 0.0..200.0      -> if (critical) DecimalFormat("&7-0.#&4❤") else DecimalFormat(
 					"&c-0.#&4❤"
 			                                                                                 )
@@ -45,7 +46,7 @@ class DamageIndicator : Listener {
 					"&5-0.#&4❤"
 			                                                                                 )
 			else               -> DecimalFormat("0.#")
-		}
+		}*/
 		val format = formatter.format(damage)
 		val colourCodes = "ea231dc6"
 		val startingIndex = floor(Math.random() * (colourCodes.length)).toInt()
@@ -104,6 +105,6 @@ class DamageIndicator : Listener {
 
 		Bukkit.getScheduler().runTaskLater(Core.plugin(), Runnable {
 			hologram.delete()
-		}, 2 * 20L)
+		}, 3 * 20L)
 	}
 }
