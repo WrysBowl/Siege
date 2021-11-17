6e200c51f3e3b40c71cf54676f545b4c4c6a256c
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-11-17 18:32:41 +0100

    Removed useless log

--- core/src/main/java/net/siegerpg/siege/core/fishing/events/FishEvent.java
+++ core/src/main/java/net/siegerpg/siege/core/fishing/events/FishEvent.java
@@ -1,25 +1,13 @@
 package net.siegerpg.siege.core.fishing.events;
 
-import net.siegerpg.siege.core.Core;
 import net.siegerpg.siege.core.fishing.FishingTask;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerFishEvent.State;
-import org.bukkit.event.server.PluginEnableEvent;
-
-import java.util.logging.Level;
 
 public class FishEvent implements Listener {
 
-	@EventHandler
-	public void onEnable(PluginEnableEvent e) {
-
-		Core
-				.plugin()
-				.getLogger()
-				.log(Level.INFO, "Baited registering baits");
-	}
 
 	@EventHandler
 	public void onFish(PlayerFishEvent e) {
