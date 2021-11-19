2d7d38da28e030e180a82470af9820bec60d2785
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-18 22:29:17 -0500

    Created Health Vial Potion

--- core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/potions/HealthVial.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/potions/HealthVial.kt
@@ -3,15 +3,18 @@ package net.siegerpg.siege.core.items.implemented.misc.potions
 import net.siegerpg.siege.core.items.enums.Rarity
 import net.siegerpg.siege.core.items.types.misc.CustomFood
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
-class TestPotion() : CustomFood(
+class HealthVial() : CustomFood(
 		name = "Health Vial",
 		customModelData = 710001,
-		description = listOf("Food to tame teachers"),
+		description = listOf("Heals you by a small amount"),
 		levelRequirement = 0,
-		material = Material.APPLE,
-		health = 20.0
+		material = Material.POTION,
+		health = 150.0
                                ) {
 
 	constructor(quality : Int) : this() {
