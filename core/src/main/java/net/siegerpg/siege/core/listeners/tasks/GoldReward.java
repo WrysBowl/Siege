4b38d92118ff027f78504d51a375c9203f696be8
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-26 12:28:18 -0500

    Gold Reward Buff

--- core/src/main/java/net/siegerpg/siege/core/listeners/tasks/GoldReward.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/tasks/GoldReward.java
@@ -30,7 +30,7 @@ public class GoldReward implements Listener {
 									                        if (p == null) return;
 									                        int level = shortIntegerPair.getFirst();
 									                        int gold = (int)
-											                        (level * 10);
+											                        (level * 20);
 									                        GoldExpListener.giveGold(p, gold);
 									                        p.sendMessage(Utils.parse(""));
 									                        p.sendMessage(Utils.parse(
