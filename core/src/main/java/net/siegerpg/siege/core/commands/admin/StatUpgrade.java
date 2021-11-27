8642b5d7b26f2f78d3ecda6a8574a88368e9742e
Wryz <57204327+Wryz@users.noreply.github.com>
2021-11-27 01:33:02 -0500

    Decreased Cost of Material Reforge

--- core/src/main/java/net/siegerpg/siege/core/commands/admin/StatUpgrade.java
+++ core/src/main/java/net/siegerpg/siege/core/commands/admin/StatUpgrade.java
@@ -156,7 +156,7 @@ public class StatUpgrade implements CommandExecutor {
 			if (entry.getValue() != 0.0 && customEquipmentMap.containsKey(entry.getKey())) {
 				Double upgradeValue = materialStatMap.get(entry.getKey());
 				Double originalValue = customEquipmentMap.get(entry.getKey());
-				sum += (int)(30+originalValue)*(30+upgradeValue);
+				sum += (int)(15+originalValue)*(15+upgradeValue);
 			}
 		}
 		this.goldCost = sum * cursorItem.getAmount();
