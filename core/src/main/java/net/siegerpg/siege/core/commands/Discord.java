efc10863aefcd959c9aae7cbf3ab6b419c9344bc
Wryz <57204327+Wryz@users.noreply.github.com>
2021-12-16 02:12:18 -0500

    Discord Recolorization of Command

--- core/src/main/java/net/siegerpg/siege/core/commands/Discord.java
+++ core/src/main/java/net/siegerpg/siege/core/commands/Discord.java
@@ -1,6 +1,7 @@
 package net.siegerpg.siege.core.commands;
 
 import net.siegerpg.siege.core.miscellaneous.Utils;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,15 +15,18 @@ public class Discord implements CommandExecutor {
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
 		Player player = (Player) sender;
-		player.sendMessage(Utils.parse("<gray><underlined>                                        "));
+		player.sendMessage("");
+		player.sendMessage(Utils.parse("<dark_gray><underlined>                                        "));
 		player.sendMessage("");
 		player.sendMessage("");
 		player.sendMessage(Utils.parse("<color:#7289da><bold><underlined>   DISCORD   <reset>"));
 		player.sendMessage("");
 		player.sendMessage(Utils.parse("<yellow><bold>\u27B2<reset> <click:open_url:https://discord.siegerpg.net><color:#99aab5>https://discord.siegerpg.net</click>"));
 		player.sendMessage("");
+		player.sendMessage(Utils.parse("<dark_gray><underlined>                                        "));
 		player.sendMessage("");
-		player.sendMessage(Utils.parse("<gray><underlined>                                        "));
+		player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1.0f, 1.5f);
+
 		return false;
 	}
 
