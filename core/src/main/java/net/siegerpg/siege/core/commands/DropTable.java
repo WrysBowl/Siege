662d5efdecfa876a74974a1b47cb32ae0aee07c0
Wryz <57204327+Wryz@users.noreply.github.com>
2021-12-16 00:36:00 -0500

    DropTable text reformatting

--- core/src/main/java/net/siegerpg/siege/core/commands/DropTable.java
+++ core/src/main/java/net/siegerpg/siege/core/commands/DropTable.java
@@ -71,32 +71,41 @@ public class DropTable implements CommandExecutor {
 		//Creating Blocks Icon
 		ItemStack blockDrops = new ItemStack(Material.GRASS_BLOCK);
 		ItemMeta blockDropsItemMeta = blockDrops.getItemMeta();
-		blockDropsItemMeta.displayName(Utils.lore("<green><bold>Block Drops"));
+		blockDropsItemMeta.displayName(Utils.lore("<color:#A4CE51><bold><underlined>Block Drops"));
 		blockDropsItemMeta.lore(new ArrayList<>() {
 			{
-				add(Utils.lore("<gray>View block drops here"));
+				add(Utils.lore(""));
+				add(Utils.lore("<gray>View drops"));
+				add(Utils.lore("<yellow>(Right Click)"));
+				add(Utils.lore("<color:#A4CE51><underlined>              "));
 			}
 		});
 		blockDrops.setItemMeta(blockDropsItemMeta);
 
 		//Creating Mob Icon
-		ItemStack mobDrops = new ItemStack(Material.LLAMA_SPAWN_EGG);
+		ItemStack mobDrops = new ItemStack(Material.SKELETON_SKULL);
 		ItemMeta mobDropsItemMeta = mobDrops.getItemMeta();
-		mobDropsItemMeta.displayName(Utils.lore("<red><bold>Mob Drops"));
+		mobDropsItemMeta.displayName(Utils.lore("<color:#E7234A><bold><underlined>Mob Drops"));
 		mobDropsItemMeta.lore(new ArrayList<>() {
 			{
-				add(Utils.lore("<gray>View mob drops here"));
+				add(Utils.lore(""));
+				add(Utils.lore("<gray>View drops"));
+				add(Utils.lore("<yellow>(Right Click)"));
+				add(Utils.lore("<color:#E7234A><underlined>              "));
 			}
 		});
 		mobDrops.setItemMeta(mobDropsItemMeta);
 
 		//Creating Boss Icon
-		ItemStack bossDrops = new ItemStack(Material.ENDERMITE_SPAWN_EGG);
+		ItemStack bossDrops = new ItemStack(Material.WITHER_SKELETON_SKULL);
 		ItemMeta bossDropsItemMeta = bossDrops.getItemMeta();
-		bossDropsItemMeta.displayName(Utils.lore("<dark_red><bold>Boss Drops"));
+		bossDropsItemMeta.displayName(Utils.lore("<color:#A22525><bold><underlined>Boss Drops"));
 		bossDropsItemMeta.lore(new ArrayList<>() {
 			{
-				add(Utils.lore("<gray>View boss drops here"));
+				add(Utils.lore(""));
+				add(Utils.lore("<gray>View drops"));
+				add(Utils.lore("<yellow>(Right Click)"));
+				add(Utils.lore("<color:#A22525><underlined>              "));
 			}
 		});
 		bossDrops.setItemMeta(bossDropsItemMeta);
@@ -165,24 +174,14 @@ public class DropTable implements CommandExecutor {
 
 			ItemMeta iconMeta = item.getItemMeta();
 			iconMeta.displayName(Utils.lore("<green>"+item.getI18NDisplayName()));
-			iconMeta.lore(new ArrayList<>() {
-				{
-					add(Utils.lore("<yellow>Gold "+
-					               String.format("%,d", blockDropTable.getGoldMin())+
-					               " <gray>- <yellow>"+
-					               String.format("%,d", blockDropTable.getGoldMax())));
-					add(Utils.lore("<light_purple>EXP "+
-					               String.format("%,d", blockDropTable.getExpMin())+
-					               " <gray>- <light_purple>"+
-					               String.format("%,d", blockDropTable.getExpMax())));
-					add(Utils.lore(""));
-					add(Utils.lore("<yellow>Click to View Drops"));
-				}
-			});
 			item.setItemMeta(iconMeta);
 
 			row.addItem(new GuiItem(item, e -> {
-				createDropsGUI(blockDropTable.getRewards()).show(player);
+				createDropsGUI(blockDropTable.getRewards(),
+				               blockDropTable.getGoldMin(),
+				               blockDropTable.getGoldMax(),
+				               blockDropTable.getExpMin(),
+				               blockDropTable.getExpMax()).show(player);
 			}));
 		}
 
@@ -199,7 +198,7 @@ public class DropTable implements CommandExecutor {
 
 		return menu;
 	}
-	private ChestGui createDropsGUI(Reward[] rewards) {
+	private ChestGui createDropsGUI(Reward[] rewards, int goldMin, int goldMax, int expMin, int expMax) {
 		//Menu
 		int size = (int) Math.ceil(rewards.length / 9.0);
 		if (size > 6) size = 6;
@@ -221,11 +220,48 @@ public class DropTable implements CommandExecutor {
 
 		//icons
 
+		//Gold Display
+		ItemStack goldDisplay = new ItemStack(Material.SUNFLOWER);
+		ItemMeta goldDisplayMeta = goldDisplay.getItemMeta();
+		goldDisplayMeta.displayName(Utils.lore("<yellow><bold><underlined>GOLD   "));
+		goldDisplayMeta.lore(new ArrayList<>() {
+			{
+				add(Utils.lore(""));
+				add(Utils.lore("<yellow>"+String.format("%,d", goldMin)+
+				               " <gray>- <yellow>"+
+				               String.format("%,d", goldMax)+
+				               " \u26C1"));
+				add(Utils.lore("<yellow><underlined>           "));
+			}
+		});
+		goldDisplay.setItemMeta(goldDisplayMeta);
+		row.addItem(new GuiItem(goldDisplay));
+
+		ItemStack expDisplay = new ItemStack(Material.EXPERIENCE_BOTTLE);
+		ItemMeta expDisplayMeta = expDisplay.getItemMeta();
+		expDisplayMeta.displayName(Utils.lore("<light_purple><bold><underlined>EXP    "));
+		expDisplayMeta.lore(new ArrayList<>() {
+			{
+				add(Utils.lore(""));
+				add(Utils.lore("<light_purple>"+String.format("%,d", expMin)+
+				               " <gray>- <light_purple>"+
+				               String.format("%,d", expMax)));
+				add(Utils.lore("<light_purple><underlined>           "));
+			}
+		});
+		expDisplay.setItemMeta(expDisplayMeta);
+		row.addItem(new GuiItem(expDisplay));
+
 		for(Reward reward : rewards) {
 			ItemStack item = reward.getItem();
 			List< Component > lore = item.lore();
 			if (lore == null) lore = new ArrayList<>();
-			lore.add(Utils.lore("<gray>Chance <yellow>"+reward.getChance()+"%"));
+
+			lore.add(Utils.lore("<dark_gray><underlined>            "));
+			lore.add(Utils.lore(""));
+			lore.add(Utils.lore("<color:#BFD57C><bold>\u2618 <reset><color:#BFD57C>"+reward.getChance()+" %"));
+			lore.add(Utils.lore("<dark_gray><underlined>            "));
+
 			item.lore(lore);
 
 			row.addItem(new GuiItem(item));
@@ -267,24 +303,14 @@ public class DropTable implements CommandExecutor {
 
 			ItemMeta iconMeta = item.getItemMeta();
 			iconMeta.displayName(Utils.lore("<green>"+key));
-			iconMeta.lore(new ArrayList<>() {
-				{
-					add(Utils.lore("<yellow>Gold "+
-					               String.format("%,d", mobDropTable.getGoldMin())+
-					               " <gray>- <yellow>"+
-					               String.format("%,d", mobDropTable.getGoldMax())));
-					add(Utils.lore("<light_purple>EXP "+
-					               String.format("%,d", mobDropTable.getExpMin())+
-					               " <gray>- <light_purple>"+
-					               String.format("%,d", mobDropTable.getExpMax())));
-					add(Utils.lore(""));
-					add(Utils.lore("<yellow>Click to View Drops"));
-				}
-			});
 			item.setItemMeta(iconMeta);
 
 			row.addItem(new GuiItem(item, e -> {
-				createDropsGUI(mobDropTable.getRewards()).show(player);
+				createDropsGUI(mobDropTable.getRewards(),
+				               mobDropTable.getGoldMin(),
+				               mobDropTable.getGoldMax(),
+				               mobDropTable.getExpMin(),
+				               mobDropTable.getExpMax()).show(player);
 			}));
 		}
 
@@ -331,24 +357,14 @@ public class DropTable implements CommandExecutor {
 
 			ItemMeta iconMeta = item.getItemMeta();
 			iconMeta.displayName(Utils.lore("<green>"+key));
-			iconMeta.lore(new ArrayList<>() {
-				{
-					add(Utils.lore("<yellow>Gold "+
-					               String.format("%,d", mobDropTable.getGoldMin())+
-					               " <gray>- <yellow>"+
-					               String.format("%,d", mobDropTable.getGoldMax())));
-					add(Utils.lore("<light_purple>EXP "+
-					               String.format("%,d", mobDropTable.getExpMin())+
-					               " <gray>- <light_purple>"+
-					               String.format("%,d", mobDropTable.getExpMax())));
-					add(Utils.lore(""));
-					add(Utils.lore("<yellow>Click to View Drops"));
-				}
-			});
 			item.setItemMeta(iconMeta);
 
 			row.addItem(new GuiItem(item, e -> {
-				createDropsGUI(mobDropTable.getRewards()).show(player);
+				createDropsGUI(mobDropTable.getRewards(),
+				               mobDropTable.getGoldMin(),
+				               mobDropTable.getGoldMax(),
+				               mobDropTable.getExpMin(),
+				               mobDropTable.getExpMax()).show(player);
 			}));
 		}
 
