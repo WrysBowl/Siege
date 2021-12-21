293e9a7e15b1c808f345e6c0a8bd3f657ae6c14a
Wryz <57204327+Wryz@users.noreply.github.com>
2021-12-20 21:51:12 -0500

    Necromancer Variant Items

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/Necromancer.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/Necromancer.java
@@ -3,9 +3,14 @@ package net.siegerpg.siege.core.drops.mobs.hillyWoods.dungeon;
 import net.siegerpg.siege.core.drops.MobDropTable;
 import net.siegerpg.siege.core.drops.Reward;
 import net.siegerpg.siege.core.items.implemented.armor.boots.ChainBoots;
+import net.siegerpg.siege.core.items.implemented.armor.boots.chainBoots.*;
 import net.siegerpg.siege.core.items.implemented.armor.chestplate.ChainChestplate;
+import net.siegerpg.siege.core.items.implemented.armor.chestplate.chainChestplates.*;
 import net.siegerpg.siege.core.items.implemented.armor.helmet.ChainHelmet;
+import net.siegerpg.siege.core.items.implemented.armor.helmet.chainHelmets.*;
 import net.siegerpg.siege.core.items.implemented.armor.leggings.ChainLeggings;
+import net.siegerpg.siege.core.items.implemented.armor.leggings.chainLeggings.*;
+import net.siegerpg.siege.core.items.implemented.misc.keys.crate.MobKey;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.blocks.MetalScrap;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Bone;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Leather;
@@ -20,8 +25,11 @@ import net.siegerpg.siege.core.items.implemented.misc.statgems.strengthGems.Simp
 import net.siegerpg.siege.core.items.implemented.misc.statgems.toughGems.PolishedToughGem;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.toughGems.SimpleToughGem;
 import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.IronAxe;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.*;
 import net.siegerpg.siege.core.items.implemented.weapons.ranged.RecurveBow;
+import net.siegerpg.siege.core.items.implemented.weapons.ranged.recurveBows.*;
 import net.siegerpg.siege.core.items.implemented.weapons.wands.EarthernWand;
+import net.siegerpg.siege.core.items.implemented.weapons.wands.earthernWands.*;
 import net.siegerpg.siege.core.miscellaneous.Utils;
 
 public class Necromancer extends MobDropTable {
@@ -64,23 +72,85 @@ public class Necromancer extends MobDropTable {
 				new Reward(new ChainLeggings(80).getUpdatedItem(false), 20.5),
 				new Reward(new ChainBoots(80).getUpdatedItem(false), 20.5),
 
+				new Reward(new ToughChainHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new ToughChainChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new ToughChainLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new ToughChainBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealingChainHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealingChainChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealingChainLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealingChainBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealthyChainHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyChainChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyChainLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyChainBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new StrongChainHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new StrongChainChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new StrongChainLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new StrongChainBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new LuckyChainHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyChainChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyChainLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyChainBoots(50).getUpdatedItem(false), 1.0),
+
 				new Reward(new ChainHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
 				new Reward(new ChainChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
 				new Reward(new ChainLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
 				new Reward(new ChainBoots(Utils.randRarity()).getUpdatedItem(false), 30.0),
-				new Reward(new ChainHelmet(Utils.randRarity()).getUpdatedItem(false), 30.0),
-				new Reward(new ChainChestplate(Utils.randRarity()).getUpdatedItem(false), 30.0),
-				new Reward(new ChainLeggings(Utils.randRarity()).getUpdatedItem(false), 30.0),
-				new Reward(new ChainBoots(Utils.randRarity()).getUpdatedItem(false), 30.0),
+
+				new Reward(new ToughChainHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughChainChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughChainLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughChainBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealingChainHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingChainChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingChainLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingChainBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealthyChainHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyChainChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyChainLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyChainBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new StrongChainHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongChainChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongChainLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongChainBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new LuckyChainHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyChainChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyChainLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyChainBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
 
 				new Reward(new IronAxe(100).getUpdatedItem(false), 10.5),
 				new Reward(new IronAxe(Utils.randRarity()).getUpdatedItem(false), 25.5),
+				new Reward(new LuckyIronAxe(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughIronAxe(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongIronAxe(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingIronAxe(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyIronAxe(Utils.randRarity()).getUpdatedItem(false), 1.0),
 
-				new Reward(new EarthernWand(100).getUpdatedItem(false), 10.5),
-				new Reward(new EarthernWand(Utils.randRarity()).getUpdatedItem(false), 25.5),
+				new Reward(new EarthernWand(Utils.randRarity()).getUpdatedItem(false), 15.0),
+				new Reward(new EarthernWand(100).getUpdatedItem(false), 5.0),
+				new Reward(new EarthernWand(80).getUpdatedItem(false), 15.0),
+				new Reward(new LuckyEarthernWand(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughEarthernWand(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongEarthernWand(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingEarthernWand(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyEarthernWand(Utils.randRarity()).getUpdatedItem(false), 1.0),
 
 				new Reward(new RecurveBow(100).getUpdatedItem(false), 10.5),
 				new Reward(new RecurveBow(Utils.randRarity()).getUpdatedItem(false), 25.5),
+				new Reward(new LuckyRecurveBow(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughRecurveBow(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongRecurveBow(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingRecurveBow(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyRecurveBow(Utils.randRarity()).getUpdatedItem(false), 1.0),
 
 				new Reward(new SimpleRegenerationGem(0).getUpdatedItem(false), 2.0),
 				new Reward(new SimpleStrengthGem(0).getUpdatedItem(false), 2.0),
@@ -92,6 +162,9 @@ public class Necromancer extends MobDropTable {
 				new Reward(new PolishedLuckGem(0).getUpdatedItem(false), 5.0),
 				new Reward(new PolishedToughGem(0).getUpdatedItem(false), 5.0),
 				new Reward(new PolishedHealthGem(0).getUpdatedItem(false), 5.0),
+
+				new Reward(new MobKey(0).getUpdatedItem(false).asQuantity(14), 100.0),
+
 				});
 	}
 
