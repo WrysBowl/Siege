a2a881e32e6d05c63e7f79929372473fc530ecc8
Wryz <57204327+Wryz@users.noreply.github.com>
2021-12-20 21:10:03 -0500

    Bull Spirit Variant Items

--- core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/BullSpirit.java
+++ core/src/main/java/net/siegerpg/siege/core/drops/mobs/hillyWoods/dungeon/BullSpirit.java
@@ -3,10 +3,15 @@ package net.siegerpg.siege.core.drops.mobs.hillyWoods.dungeon;
 import net.siegerpg.siege.core.drops.MobDropTable;
 import net.siegerpg.siege.core.drops.Reward;
 import net.siegerpg.siege.core.items.implemented.armor.boots.LeatherBoots;
+import net.siegerpg.siege.core.items.implemented.armor.boots.hardenedLeatherBoots.*;
 import net.siegerpg.siege.core.items.implemented.armor.chestplate.LeatherChestplate;
+import net.siegerpg.siege.core.items.implemented.armor.chestplate.hardenedLeatherChestplates.*;
 import net.siegerpg.siege.core.items.implemented.armor.helmet.LeatherHelmet;
+import net.siegerpg.siege.core.items.implemented.armor.helmet.hardenedLeatherHelmets.*;
 import net.siegerpg.siege.core.items.implemented.armor.leggings.LeatherLeggings;
+import net.siegerpg.siege.core.items.implemented.armor.leggings.hardenedLeatherLeggings.*;
 import net.siegerpg.siege.core.items.implemented.misc.food.Beef;
+import net.siegerpg.siege.core.items.implemented.misc.keys.crate.MobKey;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Bone;
 import net.siegerpg.siege.core.items.implemented.misc.materials.drops.mobs.Leather;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.healthGems.CrackedHealthGem;
@@ -20,6 +25,7 @@ import net.siegerpg.siege.core.items.implemented.misc.statgems.strengthGems.Flaw
 import net.siegerpg.siege.core.items.implemented.misc.statgems.toughGems.CrackedToughGem;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.toughGems.FlawedToughGem;
 import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.DoubleBladedAxe;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.doubleBladedAxes.*;
 import net.siegerpg.siege.core.miscellaneous.Utils;
 
 public class BullSpirit extends MobDropTable {
@@ -54,6 +60,31 @@ public class BullSpirit extends MobDropTable {
 				new Reward(new LeatherLeggings(80).getUpdatedItem(false), 25.5),
 				new Reward(new LeatherBoots(80).getUpdatedItem(false), 25.5),
 
+				new Reward(new ToughHardenedLeatherHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new ToughHardenedLeatherChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new ToughHardenedLeatherLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new ToughHardenedLeatherBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealingHardenedLeatherHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealingHardenedLeatherChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealingHardenedLeatherLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealingHardenedLeatherBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealthyHardenedLeatherHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyHardenedLeatherChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyHardenedLeatherLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyHardenedLeatherBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new StrongHardenedLeatherHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new StrongHardenedLeatherChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new StrongHardenedLeatherLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new StrongHardenedLeatherBoots(50).getUpdatedItem(false), 1.0),
+
+				new Reward(new LuckyHardenedLeatherHelmet(50).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyHardenedLeatherChestplate(50).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyHardenedLeatherLeggings(50).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyHardenedLeatherBoots(50).getUpdatedItem(false), 1.0),
+
 				new Reward(new LeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 20.0),
 				new Reward(new LeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 20.0),
 				new Reward(new LeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 20.0),
@@ -62,13 +93,41 @@ public class BullSpirit extends MobDropTable {
 				new Reward(new LeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 20.0),
 				new Reward(new LeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 20.0),
 				new Reward(new LeatherBoots(Utils.randRarity()).getUpdatedItem(false), 20.0),
+
+				new Reward(new ToughHardenedLeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughHardenedLeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughHardenedLeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new ToughHardenedLeatherBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealingHardenedLeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingHardenedLeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingHardenedLeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealingHardenedLeatherBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new HealthyHardenedLeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyHardenedLeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyHardenedLeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new HealthyHardenedLeatherBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new StrongHardenedLeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongHardenedLeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongHardenedLeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new StrongHardenedLeatherBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
+
+				new Reward(new LuckyHardenedLeatherHelmet(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyHardenedLeatherChestplate(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyHardenedLeatherLeggings(Utils.randRarity()).getUpdatedItem(false), 1.0),
+				new Reward(new LuckyHardenedLeatherBoots(Utils.randRarity()).getUpdatedItem(false), 1.0),
 
 				new Reward(new DoubleBladedAxe(100).getUpdatedItem(false), 7.5),
 				new Reward(new DoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 25.5),
+				new Reward(new ToughDoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 3.0),
+				new Reward(new LuckyDoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 3.0),
+				new Reward(new StrongDoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 3.0),
+				new Reward(new HealingDoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 3.0),
+				new Reward(new HealthyDoubleBladedAxe(Utils.randRarity()).getUpdatedItem(false), 3.0),
 
-				new Reward(new Beef(100)
-						           .getUpdatedItem(false)
-						           .asQuantity(16), 40.0),
+				new Reward(new Beef(100).getUpdatedItem(false).asQuantity(16), 40.0),
 
 				new Reward(new CrackedRegenerationGem(0).getUpdatedItem(false), 2.0),
 				new Reward(new CrackedStrengthGem(0).getUpdatedItem(false), 2.0),
@@ -80,6 +139,8 @@ public class BullSpirit extends MobDropTable {
 				new Reward(new FlawedLuckGem(0).getUpdatedItem(false), 5.0),
 				new Reward(new FlawedToughGem(0).getUpdatedItem(false), 5.0),
 				new Reward(new FlawedHealthGem(0).getUpdatedItem(false), 5.0),
+
+				new Reward(new MobKey(0).getUpdatedItem(false).asQuantity(6), 80.0),
 				});
 	}
 
