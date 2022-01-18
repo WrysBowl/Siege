1d0cecc34f6f65933b37eb1a9f733d6a4b69b842
Wryz <57204327+Wryz@users.noreply.github.com>
2022-01-18 15:16:28 -0500

    Revert "Possible Fix for Java Version"
    This reverts commit 3fa4e785c9a2ca7177ae95abb9b84d14465744a2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
 	}
 
 	java {
-		sourceCompatibility = JavaVersion.VERSION_16
-		targetCompatibility = JavaVersion.VERSION_16
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
 	}
 }
