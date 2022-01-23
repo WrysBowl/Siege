0b8681a5a7eb68e452d87159c7aef3b3510c50a0
Wryz <57204327+Wryz@users.noreply.github.com>
2022-01-23 16:10:41 -0500

    Fixed Straw Helmet Reward

--- core/src/main/java/net/siegerpg/siege/core/commands/admin/questRewards/rewards/GoingClubbing.java
+++ core/src/main/java/net/siegerpg/siege/core/commands/admin/questRewards/rewards/GoingClubbing.java
@@ -1,12 +1,13 @@
 package net.siegerpg.siege.core.commands.admin.questRewards.rewards;
 
 import net.siegerpg.siege.core.commands.admin.questRewards.QuestReward;
+import net.siegerpg.siege.core.items.implemented.armor.helmet.StrawHelmet;
 import org.bukkit.inventory.ItemStack;
 
 public class GoingClubbing extends QuestReward {
 
 	public GoingClubbing() {
-		super(new ItemStack[0], "goingClubbing", 100, 50);
+		super(new ItemStack[]{ new StrawHelmet(50).getUpdatedItem(false) }, "goingClubbing", 100, 50);
 	}
 
 }
--- core/src/main/java/net/siegerpg/siege/core/commands/admin/questRewards/rewards/JoeTheWeaver.java
+++ core/src/main/java/net/siegerpg/siege/core/commands/admin/questRewards/rewards/JoeTheWeaver.java
@@ -1,13 +1,12 @@
 package net.siegerpg.siege.core.commands.admin.questRewards.rewards;
 
 import net.siegerpg.siege.core.commands.admin.questRewards.QuestReward;
-import net.siegerpg.siege.core.items.implemented.armor.helmet.StrawHelmet;
 import org.bukkit.inventory.ItemStack;
 
 public class JoeTheWeaver extends QuestReward {
 
 	public JoeTheWeaver() {
-		super(new ItemStack[]{ new StrawHelmet(50).getUpdatedItem(false) }, "joeTheWeaver", 50, 20);
+		super(new ItemStack[0], "joeTheWeaver", 50, 20);
 	}
 
 }
