ca265169750572bfc99a31efb06ffaeebfb9d5d2
Wryz <57204327+Wryz@users.noreply.github.com>
2022-01-25 15:13:33 -0500

    Stats are rounded up to next integer

--- core/src/main/kotlin/net/siegerpg/siege/core/items/CustomItemUtils.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/CustomItemUtils.kt
@@ -86,6 +86,7 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.lang.reflect.Constructor
 import java.util.*
+import kotlin.math.ceil
 
 
 object CustomItemUtils {
@@ -273,7 +274,7 @@ object CustomItemUtils {
 				}
 			}
 
-			map[it] = Utils.round(totalAmount, 2)
+			map[it] = ceil(totalAmount)
 		}
 		return map
 	}
