17dc02cede0b53a60d4c56969c3cc2330b0b2d94
Wrys <wrysplays@gmail.com>
2022-02-01 22:41:00 -0500

    Hopefuly Gem Remover Fixes

--- core/src/main/java/net/siegerpg/siege/core/listeners/NPC/GemRemover.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/NPC/GemRemover.java
@@ -6,12 +6,15 @@ import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
 import com.github.stefvanschie.inventoryframework.pane.Pane;
 import net.siegerpg.siege.core.items.CustomItem;
 import net.siegerpg.siege.core.items.CustomItemUtils;
+import net.siegerpg.siege.core.items.enums.*;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.healthGems.*;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.luckGems.*;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.regenerationGems.*;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.strengthGems.*;
 import net.siegerpg.siege.core.items.implemented.misc.statgems.toughGems.*;
+import net.siegerpg.siege.core.items.implemented.weapons.melee.heavy.*;
 import net.siegerpg.siege.core.items.statgems.StatGem;
+import net.siegerpg.siege.core.items.types.misc.*;
 import net.siegerpg.siege.core.items.types.subtypes.CustomEquipment;
 import net.siegerpg.siege.core.miscellaneous.Scoreboard;
 import net.siegerpg.siege.core.miscellaneous.Utils;
@@ -32,14 +35,13 @@ import java.util.ArrayList;
 
 public class GemRemover implements Listener {
 
-	private int cost = 0;
-	private double chance = 0;
-	private ItemStack item = null;
+	private CustomItem item;
 
 	public GemRemover() {
-		cost = 0;
-		chance = 0;
-		item = null;
+		this.item = null;
+	}
+	public GemRemover(CustomItem item) {
+		this.item = item;
 	}
 
 	@EventHandler
@@ -55,52 +57,31 @@ public class GemRemover implements Listener {
 		CustomItem customItem = CustomItemUtils.INSTANCE.getCustomItem(player.getInventory().getItemInMainHand());
 		if (customItem instanceof CustomEquipment) {
 			if (((CustomEquipment) customItem).hasGem()) {
-				getMenu(player).show(player);
+				GemRemover instance = new GemRemover(customItem);
+				instance.getMenu(player).show(player);
 				return;
 			}
 		}
 		player.sendMessage(Utils.lore("<red>You must be holding an item with a gem!"));
 	}
 
-	private double calcChance() {
-
-		if (this.item == null) return 0;
-		CustomItem customItem = CustomItemUtils.INSTANCE.getCustomItem(this.item);
-		if (customItem == null) return 0;
-		if (customItem.getLevelRequirement() == null) return 0;
-		int levelReq = customItem.getLevelRequirement();
-		double calculatedChance = Utils.round(30.0 + (this.cost / ((double) (levelReq * 5))), 2);
-		if (calculatedChance > 100) return 100;
-		else return calculatedChance;
-	}
-
-	private int calcCost(double chance) {
-
-		if (this.item == null) return 0;
-		CustomItem customItem = CustomItemUtils.INSTANCE.getCustomItem(this.item);
-		if (customItem == null) return 0;
-		if (customItem.getLevelRequirement() == null) return 0;
-		int levelReq = customItem.getLevelRequirement();
-		return (int) ((chance*(levelReq * 5)) - 30.0);
-	}
-
 
 	private ChestGui getMenu(Player player) {
 
 		//Menu
-		ChestGui menu = new ChestGui(5, "Gem Remover");
+		ChestGui menu = new ChestGui(3, "Gem Remover");
 		menu.setOnGlobalClick(event -> {
 			player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 0.5f, 0.75f);
 			event.setCancelled(true);
 		});
 		menu.setOnClose(e -> {
 			if (this.item != null) {
-				e.getPlayer().getInventory().addItem(this.item);
+				player.getInventory().addItem(this.item.getUpdatedItem(false));
 				this.item = null;
 			}
 		});
 
-		OutlinePane background = new OutlinePane(0, 0, 9, 5, Pane.Priority.LOWEST);
+		OutlinePane background = new OutlinePane(0, 0, 9, 3, Pane.Priority.LOWEST);
 		ItemStack filler = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
 		ItemMeta fillerMeta = filler.getItemMeta();
 		fillerMeta.displayName(Utils.lore(""));
@@ -110,247 +91,162 @@ public class GemRemover implements Listener {
 		background.setRepeat(true);
 		menu.addPane(background);
 
-		ItemStack item = player.getInventory().getItemInMainHand();
-		this.item = item;
-		this.chance = calcChance();
-
-		OutlinePane iconDisplays = new OutlinePane(4, 2, 1, 2);
-		OutlinePane removeAdd = new OutlinePane(2, 1, 5, 1);
+		OutlinePane exitIcon = new OutlinePane(6, 1, 1, 1);
+		OutlinePane itemIcon = new OutlinePane(4, 1, 1, 1);
+		OutlinePane gemRemoveIcon = new OutlinePane(2, 1, 1, 1);
 
 		//Creating rows
-		if (this.chance < 100) {
-			removeAdd.addItem(new GuiItem(getIconRemove1000(), e -> {
-				if (this.cost < 1000) return;
-				this.cost -= 1000;
-				getMenu(player).show(player);
-				player.updateInventory();
-			}));
-			removeAdd.addItem(new GuiItem(getIconRemove100(), e -> {
-				if (this.cost < 100) return;
-				this.cost -= 100;
-				getMenu(player).show(player);
-				player.updateInventory();
-			}));
-			removeAdd.addItem(new GuiItem(getIcon()));
-			removeAdd.addItem(new GuiItem(getIconAdd100(), e -> {
-				double bal = VaultHook.econ.getBalance(player);
-				if (bal < 100) {
-					player.sendMessage(
-							Utils.tacc("&cYou do not have enough money to increase the chance!"));
-				} else {
-					this.cost += 100;
-					getMenu(player).show(player);
-					player.updateInventory();
-				}
-			}));
-			removeAdd.addItem(new GuiItem(getIconAdd1000(), e -> {
-				double bal = VaultHook.econ.getBalance(player);
-				if (bal < 1000) {
-					player.sendMessage(
-							Utils.tacc("&cYou do not have enough money to increase the chance!"));
-				} else {
-					this.cost += 1000;
-					getMenu(player).show(player);
-					player.updateInventory();
-				}
-			}));
+		exitIcon.addItem(new GuiItem(getIconExit(), e -> {
+			player.getInventory().addItem(this.item.getUpdatedItem(false));
+			player.getInventory().close();
+		}));
+		gemRemoveIcon.addItem(new GuiItem(getIconSpeedUp(), e -> {
+			getGem(player);
+		}));
+		itemIcon.addItem(new GuiItem(this.item.getUpdatedItem(false)));
 
-			iconDisplays.addItem(new GuiItem(getIconSpeedUp(), e -> {
-				double bal = VaultHook.econ.getBalance(player);
-				if (bal < calcCost(100)) {
-					player.sendMessage(
-							Utils.tacc("&cYou do not have enough money to increase the chance!"));
-				} else {
-					this.cost += calcCost(100);
-					getMenu(player).show(player);
-					player.updateInventory();
-				}
-			}));
-			iconDisplays.addItem(new GuiItem(item, e -> {
-				getGem(player);
-			}));
-		}
-		menu.addPane(removeAdd);
-		menu.addPane(iconDisplays);
+		menu.addPane(exitIcon);
+		menu.addPane(itemIcon);
+		menu.addPane(gemRemoveIcon);
 
 		return menu;
 	}
-	private ItemStack getIconRemove100() {
-		ItemStack icon = new ItemStack(Material.RED_STAINED_GLASS_PANE);
-		ItemMeta iconMeta = icon.getItemMeta();
-		iconMeta.displayName(Utils.lore("<red>-100 <yellow>\u26C1"));
-
-		icon.setItemMeta(iconMeta);
-		return icon;
-	}
-	private ItemStack getIconRemove1000() {
-		ItemStack icon = new ItemStack(Material.RED_STAINED_GLASS_PANE);
-		ItemMeta iconMeta = icon.getItemMeta();
-		iconMeta.displayName(Utils.lore("<red>-1,000 <yellow>\u26C1"));
-
-		icon.setItemMeta(iconMeta);
-		return icon;
-	}
-
-	private ItemStack getIconAdd100() {
-		ItemStack icon = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
-		ItemMeta iconMeta = icon.getItemMeta();
-		iconMeta.displayName(Utils.lore("<green>+100 <yellow>\u26C1"));
-
-		icon.setItemMeta(iconMeta);
-		return icon;
-	}
-
-	private ItemStack getIconAdd1000() {
-		ItemStack icon = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
-		ItemMeta iconMeta = icon.getItemMeta();
-		iconMeta.displayName(Utils.lore("<green>+1,000 <yellow>\u26C1"));
-
-		icon.setItemMeta(iconMeta);
-		return icon;
-	}
 
 	private ItemStack getIconSpeedUp() {
-		ItemStack icon = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+		ItemStack icon = new CrystalHammer().getUpdatedItem(false);
 		ItemMeta iconMeta = icon.getItemMeta();
-		iconMeta.displayName(Utils.lore("<green>+"+calcCost(100-chance)+" <yellow>\u26C1"));
-		icon.setItemMeta(iconMeta);
-		return icon;
-	}
-
-	private ItemStack getIcon() {
-		ItemStack icon = new ItemStack(Material.SUNFLOWER);
-		ItemMeta iconMeta = icon.getItemMeta();
-		iconMeta.displayName(Utils.lore("<yellow>Cost: " + this.cost));
+		iconMeta.displayName(Utils.lore("<color:#C0ED8E><bold>REMOVE GEM"));
 		iconMeta.lore(new ArrayList<>() {
 			{
-				add(Utils.lore("<aqua>Chance of Recovery: " + chance));
-				add(Utils.lore("<gold><bold>CLICK TO GET <reset><yellow>(maybe)"));
-
+				add(Utils.lore("<gray>Click to remove"));
+				add(Utils.lore("<gray>your gem for"));
+				add(Utils.lore("<yellow>"+String.format("%,d", getCost())+" \u26C1"));
 			}
 		});
 		icon.setItemMeta(iconMeta);
 		return icon;
 	}
 
+	private ItemStack getIconExit() {
+		ItemStack icon = new ItemStack(Material.BARRIER);
+		ItemMeta iconMeta = icon.getItemMeta();
+		iconMeta.displayName(Utils.lore("<red><bold>EXIT"));
+		icon.setItemMeta(iconMeta);
+		return icon;
+	}
+
+	private int getCost() {
+		return this.item.getSellValue();
+	}
+
 	private void getGem(Player player) {
-		if (this.item == null || this.item.getType().isAir()) return;
-		CustomItem customItem = CustomItemUtils.INSTANCE.getCustomItem(this.item);
-		if (customItem == null) return;
-		if (!(customItem instanceof CustomEquipment)) return;
-		if (!((CustomEquipment) customItem).hasGem()) return;
-		if (Utils.randTest((double) this.chance)) {
-			//How to get the stat gem from the item? Check in the stat gem listener class
-			//StatGem(itemOnCursor.statType, itemOnCursor.statAmount
-			StatGem gem = new StatGem(
-					((CustomEquipment) customItem)
-							.getStatGem()
-							.getType(), ((CustomEquipment) customItem)
-							.getStatGem()
-							.getAmount());
-			ItemStack item = new ItemStack(Material.AIR);
-			switch (gem.getType()) {
-				case HEALTH:
-					if (gem.getAmount() == 10.0) {
-						item = new RawHealthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 14.0) {
-						item = new CrackedHealthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 18.0) {
-						item = new FlawedHealthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 22.0) {
-						item = new SimpleHealthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 26.0) {
-						item = new PolishedHealthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 30.0) {
-						item = new PristineHealthGem(0).getUpdatedItem(false);
-					}
-					break;
-				case STRENGTH:
-					if (gem.getAmount() == 2.0) {
-						item = new RawStrengthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 3.0) {
-						item = new CrackedStrengthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 4.0) {
-						item = new FlawedStrengthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 6.0) {
-						item = new SimpleStrengthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 8.0) {
-						item = new PolishedStrengthGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 10.0) {
-						item = new PristineStrengthGem(0).getUpdatedItem(false);
-					}
-					break;
-				case LUCK:
-					if (gem.getAmount() == 4.0) {
-						item = new RawLuckGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 6.0) {
-						item = new CrackedLuckGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 8.0) {
-						item = new FlawedLuckGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 11.0) {
-						item = new SimpleLuckGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 14.0) {
-						item = new PolishedLuckGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 18.0) {
-						item = new PristineLuckGem(0).getUpdatedItem(false);
-					}
-					break;
-				case DEFENSE:
-					if (gem.getAmount() == 10.0) {
-						item = new RawToughGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 15.0) {
-						item = new CrackedToughGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 20.0) {
-						item = new FlawedToughGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 30.0) {
-						item = new SimpleToughGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 40.0) {
-						item = new PolishedToughGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 50.0) {
-						item = new PristineToughGem(0).getUpdatedItem(false);
-					}
-					break;
-				case REGENERATION:
-					if (gem.getAmount() == 5.0) {
-						item = new RawRegenerationGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 7.0) {
-						item = new CrackedRegenerationGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 9.0) {
-						item = new FlawedRegenerationGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 11.0) {
-						item = new SimpleRegenerationGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 13.0) {
-						item = new PolishedRegenerationGem(0).getUpdatedItem(false);
-					} else if (gem.getAmount() == 15.0) {
-						item = new PristineRegenerationGem(0).getUpdatedItem(false);
-					}
-			}
+		if (this.item == null || this.item.getItem().getType().isAir()) return;
+		if (this.item == null) return;
+		if (!(this.item instanceof CustomEquipment equipmentItem)) return;
+		if (!((CustomEquipment) this.item).hasGem()) return;
 
-			player
-					.getInventory()
-					.addItem(item);
-			player.sendMessage(Utils.tacc("&aSuccessfully removed gem!"));
-			player.playSound(
-					player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+		StatGem gem = new StatGem(
+				((CustomEquipment) this.item)
+						.getStatGem()
+						.getType(), ((CustomEquipment) this.item)
+						.getStatGem()
+						.getAmount());
 
-		} else {
-			player.sendMessage(Utils.tacc("&cThe gem broke upon trying to remove it!"));
-			player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 1.0f, 1.0f);
-		}
-		player.closeInventory();
-		VaultHook.econ.withdrawPlayer(
-				player, this.cost); //takes the cost they put in from the player
+		player.getInventory().addItem(getStatGem(gem));
+		player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+
+		VaultHook.econ.withdrawPlayer(player, getCost()); //takes the cost they put in from the player
 		Scoreboard.updateScoreboard(player);
-		CustomEquipment equipmentItem = (CustomEquipment) customItem;
-		equipmentItem.removeStatGem(); //removes stat gem
+
+		equipmentItem.removeStatGem();
 		equipmentItem.updateMeta(false);
-		Utils.giveItem(player, equipmentItem.getItem());
-		player
-				.getInventory()
-				.remove(this.item.asOne());
+		player.getInventory().remove(this.item.getItem().asOne());
+		Utils.giveItem(player, equipmentItem.getUpdatedItem(false));
+
 		player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_USE, 1.0f, 1.0f);
-		this.cost = 0;
 		this.item = null;
+	}
+
+
+	private ItemStack getStatGem(StatGem gem) {
+		ItemStack item = new ItemStack(Material.AIR);
+		switch (gem.getType()) {
+			case HEALTH:
+				if (gem.getAmount() == 10.0) {
+					item = new RawHealthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 14.0) {
+					item = new CrackedHealthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 18.0) {
+					item = new FlawedHealthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 22.0) {
+					item = new SimpleHealthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 26.0) {
+					item = new PolishedHealthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 30.0) {
+					item = new PristineHealthGem(0).getUpdatedItem(false);
+				}
+				break;
+			case STRENGTH:
+				if (gem.getAmount() == 2.0) {
+					item = new RawStrengthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 3.0) {
+					item = new CrackedStrengthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 4.0) {
+					item = new FlawedStrengthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 6.0) {
+					item = new SimpleStrengthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 8.0) {
+					item = new PolishedStrengthGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 10.0) {
+					item = new PristineStrengthGem(0).getUpdatedItem(false);
+				}
+				break;
+			case LUCK:
+				if (gem.getAmount() == 4.0) {
+					item = new RawLuckGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 6.0) {
+					item = new CrackedLuckGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 8.0) {
+					item = new FlawedLuckGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 11.0) {
+					item = new SimpleLuckGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 14.0) {
+					item = new PolishedLuckGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 18.0) {
+					item = new PristineLuckGem(0).getUpdatedItem(false);
+				}
+				break;
+			case DEFENSE:
+				if (gem.getAmount() == 10.0) {
+					item = new RawToughGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 15.0) {
+					item = new CrackedToughGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 20.0) {
+					item = new FlawedToughGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 30.0) {
+					item = new SimpleToughGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 40.0) {
+					item = new PolishedToughGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 50.0) {
+					item = new PristineToughGem(0).getUpdatedItem(false);
+				}
+				break;
+			case REGENERATION:
+				if (gem.getAmount() == 5.0) {
+					item = new RawRegenerationGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 7.0) {
+					item = new CrackedRegenerationGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 9.0) {
+					item = new FlawedRegenerationGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 11.0) {
+					item = new SimpleRegenerationGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 13.0) {
+					item = new PolishedRegenerationGem(0).getUpdatedItem(false);
+				} else if (gem.getAmount() == 15.0) {
+					item = new PristineRegenerationGem(0).getUpdatedItem(false);
+				}
+		}
+		return item;
 	}
 
 }
