113a4522bb9106dbf28d26445f1b2e7da2e13446
mcpkdotdev <98611462+mcpkdotdev@users.noreply.github.com>
2022-02-06 16:32:45 -0800

    Coded DoubleStrike.java

--- core/src/main/java/net/siegerpg/siege/core/skills/warrior/DoubleStrike.java
+++ core/src/main/java/net/siegerpg/siege/core/skills/warrior/DoubleStrike.java
@@ -1,8 +1,16 @@
 package net.siegerpg.siege.core.skills.warrior;
 
+import net.siegerpg.siege.core.*;
+import net.siegerpg.siege.core.items.*;
+import net.siegerpg.siege.core.items.enums.*;
+import net.siegerpg.siege.core.miscellaneous.*;
 import net.siegerpg.siege.core.skills.Skill;
 import net.siegerpg.siege.core.skills.SkillClass;
-import org.bukkit.entity.Player;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.potion.*;
+import org.bukkit.scheduler.*;
+import org.bukkit.util.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
@@ -56,13 +64,78 @@ public class DoubleStrike extends Skill {
 		return (int) (this.initGoldCost * level * 3.5);
 	}
 
+	// This is made because damage is repeated.
+	public void damage(Player player, Location location, double damage, LivingEntity entity) {
+
+		//spawn double slash particle effect
+		player.getWorld().spawnParticle(Particle.SWEEP_ATTACK, location, 1);
+		player.getWorld().playSound(location, Sound.ENTITY_PLAYER_ATTACK_SWEEP, 1.0f, 1.0f);
+
+		entity.damage(damage, player);
+
+	}
 
 	@Override
 	public boolean trigger(@NotNull Player player, int level) {
 		// First we check if the cooldown and mana are respected (we run the code common to all skills)
 		// If the trigger() method returns false it means that the execution was not successful (for example the cooldown wasn't finished) so we stop executing and return false
-		return super.trigger(player, level);
+		if(!super.trigger(player, level)) return false;
 
+		//used to calculate slash damage radius
+		Vector vector = player.getLocation().getDirection();
+
+		//location of particle effect
+		Location location = player.getEyeLocation().add(vector.multiply(2));
+
+		//damage to deal
+		double damage = CustomItemUtils.INSTANCE.getPlayerStat(player, StatTypes.STRENGTH);
+
+		boolean isNoEntityWeakened = true;
+
+		//actually damage entity
+		for (LivingEntity entity : location.getNearbyLivingEntities(2, 2, 2)) {
+			if (entity.equals(player)) continue;
+
+			//bukkit runnable to run when hit
+			BukkitRunnable runnable = new BukkitRunnable()
+			{
+				@Override public void run() {damage(player, location, damage, entity);}
+			};
+
+			// first damage
+			damage(player, location, damage, entity);
+
+			// second damage
+			runnable.runTaskLater(Core.plugin(),20L);
+
+
+			if (entity.hasPotionEffect(PotionEffectType.WEAKNESS)) {
+
+				isNoEntityWeakened = false;
+
+				// third damage if entity is weakened
+				runnable.runTaskLater(Core.plugin(), 40L);
+			}
+		}
+
+		//trigger end
+		if(isNoEntityWeakened){
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					triggerEnd(player, level);
+				}
+			}.runTaskLater(Core.plugin(), 20);
+		} else{
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					triggerEnd(player, level);
+				}
+			}.runTaskLater(Core.plugin(), 40);
+		}
+
+		return true;
 		// Handling of the skill goes here
 	}
 
