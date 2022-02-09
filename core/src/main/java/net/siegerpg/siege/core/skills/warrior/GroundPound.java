0062068e2168f4f1158e6bfb3acd1ba730247372
mcpkdotdev <98611462+mcpkdotdev@users.noreply.github.com>
2022-02-08 19:06:07 -0800

    Coded ground pound
    Coded ground pound. Need feedback :)

--- core/src/main/java/net/siegerpg/siege/core/skills/warrior/GroundPound.java
+++ core/src/main/java/net/siegerpg/siege/core/skills/warrior/GroundPound.java
@@ -1,13 +1,17 @@
 package net.siegerpg.siege.core.skills.warrior;
 
-import net.siegerpg.siege.core.miscellaneous.Utils;
-import net.siegerpg.siege.core.skills.Skill;
-import net.siegerpg.siege.core.skills.SkillClass;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+import net.siegerpg.siege.core.*;
+import net.siegerpg.siege.core.miscellaneous.*;
+import net.siegerpg.siege.core.skills.*;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.potion.*;
+import org.bukkit.scheduler.*;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.*;
 
-import java.time.Duration;
-import java.util.List;
+import java.time.*;
+import java.util.*;
 
 public class GroundPound extends Skill {
 
@@ -70,6 +74,77 @@ public class GroundPound extends Skill {
 	public boolean trigger(@NotNull Player player, int level) {
 		// First we check if the cooldown and mana are respected (we run the code common to all skills)
 		// If the trigger() method returns false it means that the execution was not successful (for example the cooldown wasn't finished) so we stop executing and return false
+
+		Vector vector = new Vector(0,2,0);
+		Vector fallVector = new Vector(0,-5,0); //TODO: test vector strength!
+		Location location = player.getLocation();
+
+
+
+		double damage = (100 + (getDamageMulti(level) - 1) * 100);
+
+		PotionEffect potion = new PotionEffect(PotionEffectType.WEAKNESS, 3, 0);
+
+		//launch player up!
+		player.setVelocity(vector);
+
+
+		//shockwave
+		for (LivingEntity entity : location.getNearbyLivingEntities(4, 4, 4)) {
+			if (entity.equals(player)) continue;
+
+			//push
+			entity.setVelocity(new Vector(entity.getLocation().getX()-location.getX(), entity.getLocation().getY()-location.getY(), entity.getLocation().getZ()-location.getZ()).normalize().multiply(1.5));
+
+			//damage
+			entity.damage(damage, player);
+			entity.addPotionEffect(potion);
+
+		}
+
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+
+				//make players fall fast
+				player.setVelocity(fallVector);
+			}
+		}.runTaskLater(Core.plugin(), 40L);
+
+
+		//check if player is on ground
+		new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				if(player.isOnGround()){
+
+					//shockwave
+					for (LivingEntity entity : location.getNearbyLivingEntities(4, 4, 4)) {
+						if (entity.equals(player)) continue;
+						if(!entity.isOnGround()) continue;
+
+						//push
+						entity.setVelocity(new Vector(entity.getLocation().getX()-location.getX(), entity.getLocation().getY()-location.getY(), entity.getLocation().getZ()-location.getZ()).normalize().multiply(1.5));
+
+						//damage
+						entity.damage(damage, player);
+						entity.addPotionEffect(potion);
+
+					}
+
+					//cancelation of repeation
+					this.cancel();
+				}
+			}
+		}.runTaskLaterAsynchronously(Core.plugin(), 1L);
+
+
+
+
+
+
+
 		return super.trigger(player, level);
 
 		// Handling of the skill goes here
