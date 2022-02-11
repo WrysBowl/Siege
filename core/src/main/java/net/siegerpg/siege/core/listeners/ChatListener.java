210de8645cda978ece763281966e1f4d10a53db1
Wrys <wrysplays@gmail.com>
2022-02-11 00:35:52 -0500

    CHAT IS UPDATED!

--- core/src/main/java/net/siegerpg/siege/core/listeners/ChatListener.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/ChatListener.java
@@ -1,83 +1,137 @@
 package net.siegerpg.siege.core.listeners;
 
+import io.papermc.paper.chat.*;
+import io.papermc.paper.event.player.*;
 import kotlin.Pair;
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.audience.*;
+import net.kyori.adventure.text.*;
 import net.siegerpg.siege.core.miscellaneous.*;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
+import org.bukkit.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
+import org.intellij.lang.annotations.*;
+import org.jetbrains.annotations.*;
 
-public class ChatListener implements Listener {
+public class ChatListener implements Listener, ChatRenderer {
 
 	@EventHandler
-	public void playerChat(AsyncPlayerChatEvent e) {
-
+	public void playerChat(AsyncChatEvent e) {
 		Player player = e.getPlayer();
+		Component message = e.originalMessage();
+
+		//get the level of the player
 		Pair< Short, Integer > levelExp = Levels.INSTANCE.blockingGetExpLevel(player);
 		int lvl = (levelExp != null ? levelExp.getFirst() : 0);
-		String lvlColor = LevelColor.Companion.getFromInt(lvl).getColor();
-		String level = "&8["+ lvlColor + lvl + "&8]";
-		String prefix = net.siegerpg.siege.core.miscellaneous.VaultHook.perms.getPrimaryGroup(player);
-		String message = e
-				.getMessage()
-				.replaceAll("&k", "");
-		String check = Utils.strip(message);
 
-		if (check.equalsIgnoreCase("") || check.equalsIgnoreCase(" ")) {
-			e
-					.getPlayer()
-					.sendMessage(Utils.tacc("You can not send a empty message!"));
+		//sets the level format of the message
+		String lvlColor = LevelColor.Companion.getFromInt(lvl).getColor();
+		String lvlFormatted = lvlColor+"[" + lvl + "]";
+
+		String prefix = net.siegerpg.siege.core.miscellaneous.VaultHook.perms.getPrimaryGroup(player);
+
+		//make necessary checks
+		message = replaceText(message, "&k", "");
+		message = replaceText(message, "%", "%%");
+		if (isEmpty(message)) {
+			player.sendMessage(Utils.lore("<red>You can not send a empty message!"));
 			e.setCancelled(true);
 			return;
 		}
 
+		//notify players if a player has typed their name
+		notifyName(message);
+
+		if (saidItem(message)) {
+
+			ItemStack item = player.getInventory().getItemInMainHand();
+
+			//if player has an empty hand
+			if (item.getType() == Material.AIR) {
+				player.sendMessage(Utils.lore("<red>You need an item to display!"));
+				e.setCancelled(true);
+				return;
+			}
+
+			//replace [item] with the displayed item
+			message = replaceText(message, "[item]", itemMessage(item));
+		}
+		e.message(message);
+
+		if (player.hasPermission("siege.text.format")) {
+			e.renderer((source, sourceDisplayName, msg, viewer) ->
+					           Utils.lore(lvlFormatted + " " + Utils.tacc(prefix) + " " +"<gray>"+
+					                      ((TextComponent)sourceDisplayName).content())
+					                .append(Utils.lore(" "))
+					                .append(Utils.lore(((TextComponent)msg).content())));
+		} else {
+			e.renderer((source, sourceDisplayName, msg, viewer) ->
+					           Utils.lore(lvlFormatted + " " + Utils.tacc(prefix) + " " +"<gray>"+
+					                      ((TextComponent)sourceDisplayName).content())
+					                .append(Utils.lore(" "))
+					                .append(msg));
+		}
+
+		/*
+		e.renderer().render(
+				player,
+				Utils.lore(lvlFormatted + " " + Utils.tacc(prefix) + " " +"<gray>"+player.getName()),
+				message,
+				Audience.audience(e.viewers()));
+
+		 */
+	}
+
+	private boolean saidItem(Component msg) {
+		String message = ((TextComponent)msg).content();
+		return message.contains("[item]");
+	}
+
+	private Component itemMessage(ItemStack item) {
+		String name = item.getItemMeta().getDisplayName();
+		if (name.equals("")) name = item.getI18NDisplayName();
+
+		return Utils.lore("<yellow>" + item.getAmount() + "x " + name)
+		            .hoverEvent(item);
+	}
+
+	private boolean isEmpty(Component msg) {
+		String message = ((TextComponent)msg).content();
+		String check = Utils.strip(message);
+		return check.equalsIgnoreCase("") || check.equalsIgnoreCase(" ");
+	}
+
+	private Component replaceText(Component message, @RegExp String match, String replacement) {
+		return message.replaceText(
+				TextReplacementConfig.builder()
+				                     .match(match)
+				                     .replacement(replacement).build());
+	}
+
+	private Component replaceText(Component message, @RegExp String match, Component replacement) {
+		return message.replaceText(
+				TextReplacementConfig.builder()
+				                     .match(match)
+				                     .replacement(replacement).build());
+	}
+
+	private void notifyName(Component msg) {
+		String message = ((TextComponent)msg).content();
 		for (Player p : Bukkit.getOnlinePlayers()) {
 			if (message.contains(p.getName())) {
 				p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0F, 2.0F);
 			}
 		}
+	}
 
-		if (message.contains("[item]")) {
-			e.setCancelled(true);
-			if (player
-					    .getInventory()
-					    .getItemInMainHand()
-					    .getType() != Material.AIR) {
-				ItemStack item = player
-						.getInventory()
-						.getItemInMainHand();
-				String name = item
-						.getItemMeta()
-						.getDisplayName();
-				if (name.equals(""))
-					name = player
-							.getInventory()
-							.getItemInMainHand()
-							.getI18NDisplayName();
-				Component miniMessage = Utils
-						.lore("<yellow>" + item.getAmount() + "x " + name)
-						.hoverEvent(item);
-				Component prefixes = Utils.lore(
-						Utils.tacc(level + " " + prefix + " &7" + player.getName() + " &f"));
-				Component actualMessage = Utils
-						.lore(Utils.tacc(e.getMessage()))
-						.replaceText("[item]", miniMessage);
-				Bukkit
-						.getServer()
-						.sendMessage(prefixes.append(actualMessage));
-				return;
-			}
-			player.sendMessage(Utils.lore("<red>You need an item to display!"));
-		}
-		if (player.hasPermission("siege.text.format")) {
-			e.setFormat(Utils.tacc(level + " " + prefix + " ") +
-			            Utils.tacc("&7%1$s &f" + message.replaceAll("%", "%%")));
-		} else e.setFormat(Utils.tacc(level + " " + prefix + " ") + Utils.tacc("&7%1$s &f%2$s"));
+
+	@Override
+	public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName, @NotNull Component message, @NotNull Audience viewer) {
+		return Component.text().append(sourceDisplayName)
+		                .append(Component.text(" "))
+		                .append(message).build();
 	}
 
 }
