e4fe701d86abbffc0579e6684c116486ab03c993
Wrys <wrysplays@gmail.com>
2022-03-11 14:17:43 -0500

    getStats Will Take Rebirth & Level Rewards

--- core/src/main/kotlin/net/siegerpg/siege/core/miscellaneous/StatRewards.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/miscellaneous/StatRewards.kt
@@ -2,37 +2,96 @@ package net.siegerpg.siege.core.miscellaneous
 
 import net.siegerpg.siege.core.items.enums.StatTypes
 import net.siegerpg.siege.core.levelReward.LevelReward
+import net.siegerpg.siege.core.miscellaneous.Levels.blockingGetExpLevel
+import org.bukkit.entity.Player
 
 class StatRewards {
 
-	/**
-	 * Get all stats rewarded to the player
-	 */
-	fun getLevelRewards(level : Int) : HashMap<StatTypes, Int> {
-		//get all previous stat rewards
-		val rewards : ArrayList<LevelReward> = Levels.levelRewards.subList(0, level - 2) as ArrayList<LevelReward>
+	companion object {
+		fun getRewardedStats(player : Player) : HashMap<StatTypes, Int> {
 
-		val statMap : HashMap<StatTypes, Int> = hashMapOf()
-		val statRewards : ArrayList<LevelReward> = arrayListOf()
+			val statMap : HashMap<StatTypes, Int> = hashMapOf()
 
-		if (Levels.levelRewards.size + 2 < level) return statMap//ensure that the level reward is set in the array list
+			var expLevel : Pair<Short, Int>? = blockingGetExpLevel(player)
+			if (expLevel == null) expLevel = Pair(1.toShort(), 0)
+			val level = expLevel.first.toInt()
 
-		//make a list of all rewards that are stat rewards
-		for (reward in rewards) {
-			if (reward.stats.isEmpty()) continue
-			statRewards.add(reward)
+			getLevelRewards(level).forEach { k, v ->
+				statMap.merge(k, v) { a : Int, b : Int ->
+					Integer.sum(
+							a,
+							b
+					           )
+				}
+			}
+
+			//TODO Implement getRebirthRewards
+
+			return statMap
+
+
 		}
-		if (statRewards.size < 1) return statMap //check if the stat rewards has anything in it
 
-		for (stat in statRewards) {
-			val combinedMap : HashMap<StatTypes, Int> = statMap
-			combinedMap.putAll(stat.stats)
+		/**
+		 * Get all stats rewarded to the player
+		 */
+		fun getLevelRewards(level : Int) : HashMap<StatTypes, Int> {
+			//get all previous stat rewards
+			val rewards : ArrayList<LevelReward> = Levels.levelRewards.subList(0, level - 2) as ArrayList<LevelReward>
 
-			statMap.putAll(combinedMap)
+			val statMap : HashMap<StatTypes, Int> = hashMapOf()
+			val statRewards : ArrayList<LevelReward> = arrayListOf()
+
+			if (Levels.levelRewards.size + 2 < level) return statMap//ensure that the level reward is set in the array list
+
+			//make a list of all rewards that are stat rewards
+			for (reward in rewards) {
+				if (reward.stats.isEmpty()) continue
+				statRewards.add(reward)
+			}
+			if (statRewards.size < 1) return statMap //check if the stat rewards has anything in it
+
+			for (stat in statRewards) {
+				stat.stats.forEach { k, v ->
+					statMap.merge(k, v) { a : Int, b : Int ->
+						Integer.sum(
+								a,
+								b
+						           )
+					}
+				}
+			}
+			return statMap
+
 		}
-		return statMap
 
+		/**
+		 * Get all stats rewarded to the player
+		 */
+		fun getRebirthRewards(tier : Int) : HashMap<StatTypes, Int> {
+			//get all previous stat rewards
+			val rewards : ArrayList<LevelReward> = Levels.levelRewards.subList(0, tier - 2) as ArrayList<LevelReward>
+
+			val statMap : HashMap<StatTypes, Int> = hashMapOf()
+			val statRewards : ArrayList<LevelReward> = arrayListOf()
+
+			if (Levels.levelRewards.size + 2 < tier) return statMap//ensure that the level reward is set in the array list
+
+			//make a list of all rewards that are stat rewards
+			for (reward in rewards) {
+				if (reward.stats.isEmpty()) continue
+				statRewards.add(reward)
+			}
+			if (statRewards.size < 1) return statMap //check if the stat rewards has anything in it
+
+			for (stat in statRewards) {
+				val combinedMap : HashMap<StatTypes, Int> = statMap
+				combinedMap.putAll(stat.stats)
+
+				statMap.putAll(combinedMap)
+			}
+			return statMap
+
+		}
 	}
-
-
 }
---
+++ core/src/main/kotlin/net/siegerpg/siege/core/rebirthReward/RebirthReward.kt
@@ -0,0 +1,58 @@
+package net.siegerpg.siege.core.rebirthReward
+
+import net.siegerpg.siege.core.items.enums.StatTypes
+import net.siegerpg.siege.core.listeners.GoldExpListener
+import net.siegerpg.siege.core.miscellaneous.Utils
+import net.siegerpg.siege.core.skills.SkillData
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import java.lang.String
+
+interface RebirthReward {
+
+	val level : Short
+	val gold : Int
+	val items : List<ItemStack>
+	val stats : HashMap<StatTypes, Int>
+	val skillPoints : Int
+
+
+	fun sendReward(player : Player) {
+		player.playSound(player.location, Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f)
+		player.sendTitle(
+				Utils.tacc("&5Level Up!"),
+				Utils.tacc("&d${level - 1} &7\u21E2 &5$level"),
+				10,
+				80,
+				10)
+
+		player.sendMessage("")
+		player.sendMessage(Utils.parse("<color:#BE6CD1><bold><underlined>        LEVEL UP!        <reset>"))
+		player.sendMessage(Utils.parse("<color:#CF97DD>You leveled up to level $level"))
+		player.sendMessage("")
+		if (gold > 0) {
+			player.sendMessage(Utils.parse("<yellow>+ ${String.format("%,d", gold)} \u26C1"))
+			GoldExpListener.giveGold(player, gold)
+		}
+		if (skillPoints > 0) {
+			player.sendMessage(Utils.parse("<light_blue>+ ${String.format("%,d", skillPoints)} SP"))
+			val currentPoints : Int = SkillData.getSkillPoints(player)
+			SkillData.setSkillPoints(player, currentPoints+skillPoints)
+		}
+		for (item in items) {
+			val miniMessage = Utils.lore("<gray>\u27A5 " + item.itemMeta.displayName + " <yellow>x" + item.amount).hoverEvent(item)
+			player.sendMessage(miniMessage)
+			Utils.giveItem(player, item)
+		}
+		extraReward(player)
+		player.sendMessage("")
+		player.sendMessage(Utils.parse("<color:#BE6CD1><underlined>                                  "))
+		player.sendMessage("")
+	}
+
+	fun extraReward(player : Player) {}
+	fun statMessage(player : Player, stat : StatTypes, amount : Int) {
+		player.sendMessage("+ $amount ${stat.color}$stat")
+	}
+}
