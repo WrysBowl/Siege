98b556c8bbb6b49b8d238b9aed20ca6877a67ace
Wryz <57204327+Wryz@users.noreply.github.com>
2022-03-15 17:25:28 -0400

    Added Two Custom Arrows

---
+++ core/src/main/kotlin/net/siegerpg/siege/core/items/implemented/misc/materials/drops/arrows/FlamingArrow.kt
@@ -0,0 +1,45 @@
+package net.siegerpg.siege.core.items.implemented.misc.materials.drops.arrows
+
+import net.siegerpg.siege.core.items.CustomItemUtils
+import net.siegerpg.siege.core.items.enums.Rarity
+import net.siegerpg.siege.core.items.types.misc.CustomArrow
+import net.siegerpg.siege.core.items.types.misc.CustomMaterial
+import org.bukkit.Material
+import org.bukkit.entity.Entity
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Projectile
+import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import org.bukkit.potion.PotionType
+
+class FlamingArrow() : CustomArrow(
+		name = "Flaming Arrow",
+		customModelData = 1,
+		description = listOf("Burns your target for 5 seconds"),
+		levelRequirement = 0,
+		material = Material.TIPPED_ARROW,
+                                  ) {
+	override fun getSellValue() : Int {
+		return 40
+	}
+
+	override fun targetEffect(entity : Entity) {
+		super.targetEffect(entity)
+		if (entity !is LivingEntity) return
+		val mob : LivingEntity = entity as LivingEntity
+		mob.fireTicks = 200
+	}
+
+	constructor(quality : Int) : this() {
+		this.quality = quality
+		this.rarity = Rarity.getFromInt(quality)
+		this.serialize()
+	}
+
+	constructor(item : ItemStack) : this() {
+		this.item = item
+		deserialize()
+	}
+
+}
