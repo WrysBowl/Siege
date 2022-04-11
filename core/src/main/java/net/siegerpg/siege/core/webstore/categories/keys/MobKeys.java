fe4297b53f39342f76690f9e1d0ef7616e329d32
Wrys <wrysplays@gmail.com>
2022-04-10 22:53:14 -0400

    Webstore Mob Key Addition!

---
+++ core/src/main/java/net/siegerpg/siege/core/webstore/categories/keys/MobKeys.java
@@ -0,0 +1,12 @@
+package net.siegerpg.siege.core.webstore.categories.keys;
+
+import net.siegerpg.siege.core.items.implemented.misc.keys.crate.*;
+
+public class MobKeys extends Keys {
+
+	public MobKeys() {
+
+		super("key", "mob", 1, new MobKey());
+	}
+
+}
