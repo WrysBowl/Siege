b492e754ac9b1830dafc6ccd5433a05e8a2a9655
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2022-05-26 15:22:49 +0200

    Updated db to use the new system

--- core/src/main/kotlin/net/siegerpg/siege/core/database/DatabaseManager.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/database/DatabaseManager.kt
@@ -1,49 +1,48 @@
 package net.siegerpg.siege.core.database
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
 import net.siegerpg.siege.core.Core
-import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
 import java.sql.Connection
+import java.sql.DriverManager
 import java.sql.SQLException
 
 object DatabaseManager {
 
-	private var url : String = ""
-	private var user : String = ""
-	private var password : String = ""
 
-	private val config = HikariConfig()
-	private val ds : HikariDataSource
+	private val dbFile : File = File(Core.plugin().dataFolder.absolutePath, "data.db")
 
 	init {
+		try {
+			if (!dbFile.exists()) {
+				dbFile.createNewFile();
+			}
 
-		val configFile = File(Core.plugin().dataFolder.absolutePath, "privKeys.yml")
-		if (!configFile.exists()) {
-			Core.plugin().logger.severe("privKeys.yml not found")
+			Class.forName("org.sqlite.JDBC")
+			val connection = DriverManager.getConnection("jdbc:sqlite:$dbFile")
+			val s = connection.createStatement()
+			s.executeUpdate(
+					"CREATE TABLE IF NOT EXISTS userData (uuid varchar(64), skill_points int, skill_data text, level int DEFAULT 1, experience int)"
+			               )
+			s.executeUpdate(
+					"CREATE TABLE IF NOT EXISTS ipData (uuid varchar(64), ip text)"
+			               )
+			s.executeUpdate(
+					"CREATE TABLE IF NOT EXISTS webstoreData (uuid varchar(64), command text)"
+			               )
+			s.executeUpdate(
+					"CREATE TABLE IF NOT EXISTS bossData (playerID varchar(64), bossName text, timeTaken int, percentageDone int)"
+			               )
+
+			s.close()
+		} catch (e : Exception) {
+			e.printStackTrace()
 		}
-		val configuration : YamlConfiguration =
-				YamlConfiguration.loadConfiguration(configFile)
-		url = String.format(
-				"jdbc:mysql://%s/%s",
-				configuration.getString("db.endpoint"),
-				configuration.getString("db.dbname")
-		                   )
-		configuration.getString("db.username")?.let { user = it }
-		configuration.getString("db.password")?.let { password = it }
-		config.jdbcUrl = url
-		config.username = user
-		config.password = password
-		config.addDataSourceProperty("cachePrepStmts", "true")
-		config.addDataSourceProperty("prepStmtCacheSize", "250")
-		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-		config.leakDetectionThreshold = 30000;
-		ds = HikariDataSource(config)
+
+
 	}
 
 	@Throws(SQLException::class)
 	fun getConnection() : Connection? {
-		return ds.connection
+		return DriverManager.getConnection("jdbc:sqlite:$dbFile")
 	}
 }
