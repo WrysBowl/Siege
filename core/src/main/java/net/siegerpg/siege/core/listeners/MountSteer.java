376145a23c0ea04420b0e1078c07aeea7154794c
Jack <jfernell123@gmail.com>
2022-05-30 16:35:41 +0100

    Fixed Mount Cooldowns
    -Potentially fixed the cooldown problem
    -Increased cooldown to 2 seconds incase of inaccuracy
    -Added cooldown message if player is still in cooldown

--- core/src/main/java/net/siegerpg/siege/core/listeners/MountSteer.java
+++ core/src/main/java/net/siegerpg/siege/core/listeners/MountSteer.java
@@ -141,6 +141,11 @@ public class MountSteer extends PacketListenerAbstract implements Listener {
 		Player player = e.getPlayer();
 		ItemStack item = e.getItem();
 
+		//start cooldown for player
+		if (!currentCooldown.containsKey(player)) {
+			currentCooldown.put(player, System.currentTimeMillis());
+		}
+
 		if(!isSpawnEgg(item)) return;
 		EntityType type = getSpawnEggType(item);
 		if (type==null) return;
@@ -156,7 +161,7 @@ public class MountSteer extends PacketListenerAbstract implements Listener {
 		}
 
 		try {
-
+			
 			if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
 			if (player.isSneaking()) {
 				player.getWorld().spawnParticle(Particle.CAMPFIRE_COSY_SMOKE,player.getLocation(),10);
@@ -165,14 +170,12 @@ public class MountSteer extends PacketListenerAbstract implements Listener {
 
 			//cooldown
 			if (currentCooldown.containsKey(player)) {
-
-				if ((System.currentTimeMillis() - currentCooldown.get(player)) >= 1000) {
-					currentCooldown.put(player, System.currentTimeMillis());
+				if ((System.currentTimeMillis() - currentCooldown.get(player)) >= 2000) {
+					currentCooldown.remove(player);
 				} else {
+					player.sendMessage(Utils.lore("<red>Your mount is still on cooldown!"));
 					return;
 				}
-			} else {
-				currentCooldown.put(player, System.currentTimeMillis());
 			}
 
 			String entityName = item.getItemMeta().getDisplayName();
