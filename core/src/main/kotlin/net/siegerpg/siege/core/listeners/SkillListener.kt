aa14bdd07b9e724a85a2160cccf627478c33f532
Wryz <57204327+Wryz@users.noreply.github.com>
2022-06-08 21:20:21 -0400

    Merge/Unmerge skills like cosmetics

--- core/src/main/kotlin/net/siegerpg/siege/core/listeners/SkillListener.kt
+++ core/src/main/kotlin/net/siegerpg/siege/core/listeners/SkillListener.kt
@@ -1,21 +1,20 @@
 package net.siegerpg.siege.core.listeners
 
 import net.siegerpg.siege.core.items.CustomItem
-import net.siegerpg.siege.core.items.CustomItemUtils
 import net.siegerpg.siege.core.items.CustomItemUtils.getCustomItem
-import net.siegerpg.siege.core.items.statgems.StatGem
+import net.siegerpg.siege.core.items.types.armor.CustomHelmet
 import net.siegerpg.siege.core.items.types.misc.CustomSkill
-import net.siegerpg.siege.core.items.types.misc.StatGemType
-import net.siegerpg.siege.core.items.types.subtypes.CustomEquipment
+import net.siegerpg.siege.core.items.types.subtypes.CustomCosmetic
 import net.siegerpg.siege.core.items.types.subtypes.CustomWeapon
 import net.siegerpg.siege.core.items.types.weapons.CustomBow
 import net.siegerpg.siege.core.items.types.weapons.CustomMeleeWeapon
 import net.siegerpg.siege.core.items.types.weapons.CustomWand
 import net.siegerpg.siege.core.listeners.NPC.Herbert
-import net.siegerpg.siege.core.miscellaneous.Levels
 import net.siegerpg.siege.core.miscellaneous.Scoreboard
 import net.siegerpg.siege.core.miscellaneous.VaultHook
 import net.siegerpg.siege.core.miscellaneous.sendMiniMessage
+import net.siegerpg.siege.core.skills.Skill
+import net.siegerpg.siege.core.skills.SkillClass
 import net.siegerpg.siege.core.skills.SkillData
 import org.bukkit.Sound
 import org.bukkit.entity.Player
@@ -24,6 +23,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.InventoryAction
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.player.PlayerInteractEvent
 import kotlin.String
 
@@ -35,76 +35,76 @@ class SkillListener : Listener {
 		if (e.whoClicked !is Player) return
 		if (e.action != InventoryAction.SWAP_WITH_CURSOR) return
 		val player = e.whoClicked as Player
-		val itemOnCursor = getCustomItem(e.cursor)
-		val itemInteractedWith = getCustomItem(e.currentItem)
-		if (itemOnCursor == null || itemInteractedWith == null) return
-		if (itemOnCursor !is CustomSkill) return
-		if (itemInteractedWith !is CustomWeapon) return
-		if (!itemInteractedWith.canAddSkill()) {
-			player.sendMiniMessage("<red>That skill can not be merged onto this item!")
-			return
-		}
-		if (!SkillData.hasSkillUnlocked(player, itemOnCursor.skill)) {
-			player.sendMiniMessage("<red>You do not have this skill unlocked!")
-			return
-		}
-		if (itemInteractedWith.hasSkill(itemOnCursor)) {
-			player.sendMiniMessage("<red>This skill has already been applied.")
-			return
-		}
-		val cost = (Herbert.getSellValue(e.currentItem) * 5).toInt()
-		if (VaultHook.econ.getBalance(player) < cost) {
-			player.sendMiniMessage("<red>You need ${String.format("%,d", cost)} \u26C1 to apply this book!")
-			return
-		}
-		VaultHook.econ.withdrawPlayer(player, cost.toDouble())
-		Scoreboard.updateScoreboard(player)
+		val itemOnCursor = getCustomItem(e.cursor) //cosmetic helmet
+		val itemInteractedWith = getCustomItem(e.currentItem) //helmet
+		if (itemOnCursor == null && itemInteractedWith != null) { //remove cosmetic from helmet
+			if (e.action != InventoryAction.PICKUP_HALF) return
+			if (itemOnCursor !is CustomSkill) return
+			if (itemInteractedWith !is CustomWeapon) return
+			if (!e.inventory.type.equals(InventoryType.CRAFTING)) return
 
-		player.playSound(
-				player.location,
-				Sound.BLOCK_CHORUS_FLOWER_DEATH,
-				0.8f,
-				1.2f)
+			//set clicked item's original values back
+			itemInteractedWith.removeSkill()
+			itemInteractedWith.updateMeta(false)
 
-		itemInteractedWith.addSkill(itemOnCursor)
-		itemInteractedWith.updateMeta(false)
-		e.currentItem = itemInteractedWith.item
-		e.isCancelled = true
-		val newItem = e.cursor
-		if (newItem != null) {
-			newItem.amount = newItem.amount - 1
+			e.currentItem = itemInteractedWith.getUpdatedItem(false) //change clicked item to the new cosmetic item
+			e.isCancelled = true
+			player.setItemOnCursor(itemInteractedWith.customSkill?.getUpdatedItem(false))
+
+		} else if (itemOnCursor != null && itemInteractedWith != null) { //fusing held cosmetic to clicked item
+			if (e.action != InventoryAction.SWAP_WITH_CURSOR) return
+			if (itemOnCursor !is CustomSkill) return
+			if (itemInteractedWith !is CustomWeapon) return
+			if (!itemInteractedWith.hasSkill()) {
+				player.sendMiniMessage("<red>That skill can not be merged onto this item!")
+				return
+			}
+			if (!SkillData.hasSkillUnlocked(player, itemOnCursor.skill)) {
+				player.sendMiniMessage("<red>You do not have this skill unlocked!")
+				return
+			}
+			/*
+			val cost = Herbert.getSellValue(e.currentItem)
+			if (VaultHook.econ.getBalance(player) < Herbert.getSellValue(e.currentItem)) {
+				player.sendMiniMessage("<red>You need ${String.format("%,d", cost)} \u26C1 to apply this book!")
+				return
+			}
+			VaultHook.econ.withdrawPlayer(player, cost.toDouble())
+			Scoreboard.updateScoreboard(player)
+			 */
+
+			player.playSound(
+					player.location,
+					Sound.BLOCK_CHORUS_FLOWER_DEATH,
+					0.8f,
+					1.2f)
+
+			itemInteractedWith.addSkill(itemOnCursor)
+			itemInteractedWith.updateMeta(false)
+			e.currentItem = itemInteractedWith.item
+			e.isCancelled = true
+			val newItem = e.cursor
+			if (newItem != null) {
+				newItem.amount = newItem.amount - 1
+			}
+			player.setItemOnCursor(newItem)
 		}
-		player.setItemOnCursor(newItem)
 	}
 
 	@EventHandler
 	fun onInteractEvent(e : PlayerInteractEvent) {
-		val action : Action = e.action
 		val item : CustomItem = getCustomItem(e.player.inventory.itemInMainHand) ?: return
-		item.let {
-			if (it !is CustomWeapon) {
-				return
-			}
-			if (it is CustomBow) {
-				if (action.equals(Action.LEFT_CLICK_BLOCK) || action.equals(Action.LEFT_CLICK_AIR)) {
-					it.skillBooks.forEach {
-						if (it != null) {
-							it.skill.trigger(e.player)
-						}
-					}
-				}
-			}
+		if (item !is CustomWeapon) return
 
-			if (it is CustomWand || it is CustomMeleeWeapon) {
-				if (action.equals(Action.RIGHT_CLICK_BLOCK) || action.equals(Action.RIGHT_CLICK_AIR)) {
-					it.skillBooks.forEach {
-						if (it != null) {
-							it.skill.trigger(e.player)
-						}
-					}
-				}
-			}
-		}
+		val customSkill : CustomSkill = item.customSkill ?: return
+		val skillClass : SkillClass = customSkill.skill.skillClass
+		val actions : List<Action> = skillClass.clickActions
+		val action : Action = e.action
+
+		if (!actions.contains(action)) return
+
+		//trigger
+		customSkill.skill.trigger(e.player)
 	}
 
 }
